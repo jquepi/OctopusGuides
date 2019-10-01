71af79b33ec0c4cd8f6052cf86116c3b1a03f757
Matthew Casperson <matthewcasperson@gmail.com>
2019-10-01 11:14:45 +1000

    Start work on installing azure devops

---
+++ ports.md
@@ -0,0 +1,10 @@
+These are the ports used by the various apps deployed by Puppet and Octopus.
+
+| Port | App |
+|-|-|
+| 80   | Octopus      |
+| 8080 | Jenkins      |
+| 8081 | ISS Dev      |
+| 8082 | ISS Test     |
+| 8083 | ISS Prod     |
+| 9090 | Azure Devops |
--- puppet/azuredevops.pp
+++ puppet/azuredevops.pp
@@ -3,3 +3,18 @@ download_file { 'azuredevopsexpress2019.exe':
   url                   =>
     'https://octopus-guides.s3.amazonaws.com/azuredevops/azuredevopsexpress2019.exe',
 }
+
+exec { 'Install Azure Devops':
+  command => 'C:\\tools\\azuredevopsexpress2019.exe /silent '
+}
+
+exec { 'Deploy Azure Devops':
+  provider => 'powershell',
+  command => '& "C:\\Program Files\\Azure DevOps Server 2019\\Tools\\TfsConfig" unattend /unattendfile:.\\config\\azuredevops.ini /continue'
+}
+
+user { 'azuredevops':
+  ensure => present,
+  password => 'Password01!',
+  groups => 'Administrators'
+}
