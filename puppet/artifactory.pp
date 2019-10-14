57018d9c3e639df53f537506fe6152bc7f2f1fcb
Matthew Casperson <matthewcasperson@gmail.com>
2019-10-14 14:35:49 +1000

    Abandoned docker with Linux containers, installed artifactory directly

--- puppet/artifactory.pp
+++ puppet/artifactory.pp
@@ -1,3 +1,24 @@
-docker::image { 'ubuntu':
-  image_tag => 'precise'
+archive { 'C:/install/jfrog-artifactory-oss-6.13.1.zip':
+  ensure       => present,
+  extract      => true,
+  extract_path => 'C:/install',
+  source       => 'https://octopus-guides.s3.amazonaws.com/artifactory/jfrog-artifactory-oss-6.13.1.zip',
+  creates      => 'C:/install/artifactory-oss-6.13.1/README.txt',
+  cleanup      => true,
+}
+-> file { 'C:/install_artifactory.ps1':
+  ensure  => 'file',
+  owner   => 'Administrators',
+  group   => 'Administrators',
+  mode    => '0644',
+  content => @(EOT)
+    start "C:\install\artifactory-oss-6.13.1\bin\artifactory.bat" -PassThru
+    New-Item -ItemType file c:\ArtifactoryStarted.txt
+    | EOT
+}
+-> exec { 'Install Artifactory':
+  command  => '& C:/install_artifactory.ps',
+  creates  => 'c:/ArtifactoryStarted.txt',
+  timeout  => 3600,
+  provider => powershell,
 }
