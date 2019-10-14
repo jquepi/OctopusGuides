8ba444d32b5c0bd57b15b77848b397f62bc36304
Matthew Casperson <matthewcasperson@gmail.com>
2019-10-14 13:38:58 +1000

    Switched to linux containers

--- puppet/docker.pp
+++ puppet/docker.pp
@@ -2,3 +2,7 @@ package { 'docker-for-windows':
   ensure   => installed,
   provider => chocolatey
 }
+-> exec { 'Switch to Linux containers':
+  provider => 'powershell',
+  command  => '& $Env:ProgramFiles\Docker\Docker\DockerCli.exe -SwitchDaemon'
+}
