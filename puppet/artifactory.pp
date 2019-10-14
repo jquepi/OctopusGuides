25daa9fff3412b453e99b8f03e078a27ea5d5007
Matthew Casperson <matthewcasperson@gmail.com>
2019-10-15 07:54:46 +1000

    Fixed up the regex

--- puppet/artifactory.pp
+++ puppet/artifactory.pp
@@ -8,8 +8,8 @@ archive { 'C:/install/jfrog-artifactory-oss-6.13.1.zip':
 }
 -> file_line { 'installStateName':
   path    => 'C:/install/artifactory-pro-6.13.1/tomcat/conf/server.xml',
-  line    => '        <Connector port="8041" sendReasonPhrase="true" relaxedPathChars='\[\]' relaxedQueryChars='\[\]' maxThreads="200"/>',
-  match   => '^\s*<Connector port="8081" sendReasonPhrase="true" relaxedPathChars='\[\]' relaxedQueryChars='\[\]' maxThreads="200"/>',
+  line    => '        <Connector port="8041" sendReasonPhrase="true" relaxedPathChars=\'[]\' relaxedQueryChars=\'[]\' maxThreads="200"/>',
+  match   => '^\s*<Connector port="8081" sendReasonPhrase="true" relaxedPathChars=\'\[\]\' relaxedQueryChars=\'\[\]\' maxThreads="200"/>',
   replace => true,
 }
 -> file { 'C:/install_artifactory.ps1':
@@ -19,6 +19,7 @@ archive { 'C:/install/jfrog-artifactory-oss-6.13.1.zip':
   mode    => '0644',
   content => @(EOT)
     start "C:\install\artifactory-pro-6.13.1\bin\artifactory.bat" -PassThru
+    nuget sources add -Name Artifactory -Source http://localhost:8041/artifactory/api/nuget/NuGet -Username admin -Password password
     New-Item -ItemType file c:\ArtifactoryStarted.txt
     | EOT
 }
