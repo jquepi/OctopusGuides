363f5fc99ede204ce7157f456fbf9b01dd75067c
Matthew Casperson <matthewcasperson@gmail.com>
2019-10-28 09:02:03 +1000

    Fixing bamboo deployment

--- create-azure-web-app.ps1
+++ create-azure-web-app.ps1
@@ -2,16 +2,27 @@ param([String] $name)
 
 $exists = & az group exists --name $name
 if ($exists -eq "false") {
+    Write-Host "Resource group $name does not exist. Creating..."
     az group create --location australiaeast --name $name --tags Lifetime=Permanent OwnerContact=@matthew.casperson
+} else {
+    Write-Host "Resource group $name already exists."
 }
 
 $plans = & az appservice plan list --resource-group $name | ConvertFrom-Json
 if ($plans.Count -eq 0) {
+    Write-Host "App service plan $name does not exist. Creating..."
     az appservice plan create --name $name --resource-group $name --number-of-workers 1 --sku B1
+
+}
+else {
+    Write-Host "Resource group $name already exists."
 }
 
 $apps = az webapp list --resource-group $name | ConvertFrom-Json
 if ($apps.Count -eq 0) {
-az webapp create -g $name -p $name -n $name --% --runtime "aspnet|V4.7"
+    Write-Host "Webapp $name does not exist. Creating..."
+    az webapp create -g $name -p $name -n $name --% --runtime "aspnet|V4.7"
     az webapp log config -g $name -n $name --application-logging true --detailed-error-messages true --web-server-logging filesystem
+} else {
+    Write-Host "Webapp $name already exists."
 }
