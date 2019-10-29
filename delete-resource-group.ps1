6755f8103142be13841e1ddff081cf58502af266
Matthew Casperson <matthewcasperson@gmail.com>
2019-10-30 08:46:55 +1000

    Don't wait for the group to be deleted

--- delete-resource-group.ps1
+++ delete-resource-group.ps1
@@ -1,5 +1,5 @@
 param([String] $name)
 $exists = & az group exists --name $name
 if ($exists -eq "true") {
-    az group delete --name $name --yes
+    az group delete --name $name --yes --no-wait
 }
