6d7928a8d9dd01f687b3902bf8c34afca2c1771d
Matthew Casperson <matthewcasperson@gmail.com>
2019-11-11 07:25:09 +1000

    Updated azure devops to 2019 1.1

--- puppet/azuredevops.pp
+++ puppet/azuredevops.pp
@@ -9,10 +9,10 @@ package { 'sql-server-express':
 -> file { 'C:/tools':
   ensure => 'directory'
 }
--> download_file { 'azuredevopsexpress2019.exe':
+-> download_file { 'azuredevopsserver2019.1.1_rc.exe':
   destination_directory => 'C:/tools',
   url                   =>
-    'https://octopus-guides.s3.amazonaws.com/azuredevops/azuredevopsexpress2019.exe',
+    'https://octopus-guides.s3.amazonaws.com/azuredevops/azuredevopsserver2019.1.1_rc.exe',
 }
 -> file { 'C:/install_azure.ps1':
   ensure  => 'file',
@@ -20,7 +20,7 @@ package { 'sql-server-express':
   group   => 'Administrators',
   mode    => '0644',
   content => @(EOT)
-    Start-Process "C:\tools\azuredevopsexpress2019.exe" -ArgumentList @("/silent") -NoNewWindow -Wait
+    Start-Process "C:\tools\azuredevopsserver2019.1.1_rc.exe" -ArgumentList @("/silent") -NoNewWindow -Wait
     Start-Process "C:\Program Files\Azure DevOps Server 2019\Tools\TfsConfig.exe" -ArgumentList @("unattend", "/unattendfile:.\config\azuredevops.ini", "/continue") -NoNewWindow -Wait
     New-Item -ItemType file c:\AzureDevOpsStarted.txt
     | EOT
