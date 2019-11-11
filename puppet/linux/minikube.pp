72364143c7f6b7b58ff5a05a731ebf638d74f840
Matthew Casperson <matthewcasperson@gmail.com>
2019-11-12 08:13:05 +1000

    Still working on linux build

--- puppet/linux/minikube.pp
+++ puppet/linux/minikube.pp
@@ -8,7 +8,7 @@ apt::source { 'docker':
   comment  => 'This is the docker repository',
   location => 'https://download.docker.com/linux/ubuntu',
   release  => '',
-  repos    => 'stable bionic',
+  repos    => 'bionic',
   key      => {
     'id' => '9DC858229FC7DD38854AE2D88D81803C0EBFCD88',
   },
