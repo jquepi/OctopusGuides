7a3726aa2551ce5fb50c39106ca29cfc63a3b566
Matthew Casperson <matthewcasperson@gmail.com>
2019-11-12 07:36:37 +1000

    Updated apt repo

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 wget https://apt.puppetlabs.com/puppet6-release-bionic.deb
 sudo dpkg -i puppet6-release-bionic.deb
+sudo apt-get update
 sudo apt-get install puppet-agent
 
 sudo puppet module install module puppetlabs/apt
