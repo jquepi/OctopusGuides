fd83d4cf6a14ce9d5f74fed9302cadffb45deb70
Matthew Casperson <matthewcasperson@gmail.com>
2019-11-12 08:16:41 +1000

    Still working on linux build

--- puppet/linux/minikube.pp
+++ puppet/linux/minikube.pp
@@ -3,8 +3,7 @@ apt::key { 'docker-repository':
   source => 'https://download.docker.com/linux/ubuntu/gpg',
   server  => 'pgp.mit.edu'
 }
-
-apt::source { 'docker':
+-> apt::source { 'docker':
   comment  => 'This is the docker repository',
   location => 'https://download.docker.com/linux/ubuntu',
   release  => '',
@@ -16,59 +15,31 @@ apt::source { 'docker':
     'deb' => true,
   },
 }
-
-apt::key { 'kubernetes-repository':
-  id     => '54A647F9048D5688D7DA2ABE6A030B21BA07F4FB',
-  source => 'https://packages.cloud.google.com/apt/doc/apt-key.gpg',
-  server  => 'pgp.mit.edu'
-}
-
-apt::source { 'kubernetes':
-  comment  => 'This is the kubernetes repository',
-  location => 'http://apt.kubernetes.io/',
-  release  => '',
-  repos    => 'kubernetes-xenial main',
-  key      => {
-    'id' => '54A647F9048D5688D7DA2ABE6A030B21BA07F4FB',
-  },
-  include  => {
-    'deb' => true,
-  },
-}
-
-package { 'apt-transport-https':
+-> package { 'apt-transport-https':
   ensure => installed,
 }
-
-package { 'ca-certificates':
+-> package { 'ca-certificates':
   ensure => installed,
 }
-
-package { 'curl':
+-> package { 'curl':
   ensure => installed,
 }
-
-package { 'gnupg-agent':
+-> package { 'gnupg-agent':
   ensure => installed,
 }
-
-package { 'software-properties-common':
+-> package { 'software-properties-common':
   ensure => installed,
 }
-
-package { 'docker-ce':
+-> package { 'docker-ce':
   ensure => installed,
 }
-
-package { 'docker-ce-cli':
+-> package { 'docker-ce-cli':
   ensure => installed,
 }
-
-package { 'containerd.io':
+-> package { 'containerd.io':
   ensure => installed,
 }
-
-file { '/usr/local/bin/minikube':
+-> file { '/usr/local/bin/minikube':
   source => 'https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64',
   mode   => '0755'
 }
@@ -88,6 +59,23 @@ file { '/usr/local/bin/minikube':
   logoutput => true
 }
 
-package { 'kubectl':
+apt::key { 'kubernetes-repository':
+  id     => '54A647F9048D5688D7DA2ABE6A030B21BA07F4FB',
+  source => 'https://packages.cloud.google.com/apt/doc/apt-key.gpg',
+  server  => 'pgp.mit.edu'
+}
+-> apt::source { 'kubernetes':
+  comment  => 'This is the kubernetes repository',
+  location => 'http://apt.kubernetes.io/',
+  release  => '',
+  repos    => 'kubernetes-xenial main',
+  key      => {
+    'id' => '54A647F9048D5688D7DA2ABE6A030B21BA07F4FB',
+  },
+  include  => {
+    'deb' => true,
+  },
+}
+-> package { 'kubectl':
   ensure => installed,
 }
