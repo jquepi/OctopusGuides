6df1d07f407e34fa698e2b3a54a4d12e160fe0ee
Matthew Casperson <matthewcasperson@gmail.com>
2019-11-12 09:05:37 +1000

    Creating the pfx file

--- puppet/linux/minikube.pp
+++ puppet/linux/minikube.pp
@@ -41,29 +41,6 @@ apt::key { 'docker-repository':
   ensure => installed,
 }
 */
-
-file { '/usr/local/bin/minikube':
-  source => 'https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64',
-  mode   => '0755'
-}
--> file { '/opt/start-minikube.sh':
-  ensure  => 'file',
-  mode    => '0744',
-  content => @(EOT)
-    #!/bin/bash
-    # The GitHub Actions VM has a custom Docker version like 3.0.8, and minikube doesn't understand this version,
-    # so we ignore the system verification errors
-    sudo minikube start --vm-driver=none --extra-config kubeadm.ignore-preflight-errors=SystemVerification
-    touch /opt/minikube-started
-    | EOT
-}
--> exec { 'Start minikube':
-  command   => '/opt/start-minikube.sh',
-  creates   => '/opt/minikube-started',
-  timeout   => 3600,
-  logoutput => true
-}
-
 apt::key { 'kubernetes-repository':
   id     => '54A647F9048D5688D7DA2ABE6A030B21BA07F4FB',
   source => 'https://packages.cloud.google.com/apt/doc/apt-key.gpg',
@@ -84,3 +61,38 @@ apt::key { 'kubernetes-repository':
 -> package { 'kubectl':
   ensure => installed,
 }
+-> package { 'openssl':
+  ensure => installed,
+}
+-> file { '/usr/local/bin/minikube':
+  source => 'https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64',
+  mode   => '0755'
+}
+-> file { '/opt/start-minikube.sh':
+  ensure  => 'file',
+  mode    => '0744',
+  content => @(EOT)
+    #!/bin/bash
+    # The GitHub Actions VM has a custom Docker version like 3.0.8, and minikube doesn't understand this version,
+    # so we ignore the system verification errors
+    sudo minikube start --vm-driver=none --extra-config kubeadm.ignore-preflight-errors=SystemVerification
+    sudo cat ~/.kube/config
+    sudo kubectl get nodes
+    # Create a pfx file with the client certificate and key
+    openssl pkcs12 \
+      -passout pass: \
+      -export \
+      -out ~/client.pfx \
+      -in ~/.minikube/client.crt \
+      -inkey ~/.minikube/client.key
+    touch /opt/minikube-started
+    | EOT
+}
+-> exec { 'Start minikube':
+  command   => '/opt/start-minikube.sh',
+  creates   => '/opt/minikube-started',
+  timeout   => 3600,
+  logoutput => true
+}
+
+
