a8abac9457a587cb9e88f69ce7c495461d420966
Matthew Casperson <matthewcasperson@gmail.com>
2019-11-11 19:19:17 +1000

    Used x64 agent

--- puppet/azuredevopsagent.pp
+++ puppet/azuredevopsagent.pp
@@ -1,12 +1,12 @@
-file { 'C:/tools/vsts-agent-win-x86-2.144.2':
+file { 'C:/tools/vsts-agent':
   ensure => 'directory'
 }
--> archive { 'C:/tools/vsts-agent-win-x86-2.144.2.zip':
+-> archive { 'C:/tools/vsts-agent-win-x64-2.160.0.zip':
   ensure       => present,
   extract      => true,
-  extract_path => 'C:/tools/vsts-agent-win-x86-2.144.2',
-  source       => 'https://go.microsoft.com/fwlink/?linkid=2066763',
-  creates      => 'C:/tools/vsts-agent-win-x86-2.144.2/run.cmd',
+  extract_path => 'C:/tools/vsts-agent',
+  source       => 'https://vstsagentpackage.azureedge.net/agent/2.160.0/vsts-agent-win-x64-2.160.0.zip',
+  creates      => 'C:/tools/vsts-agent/run.cmd',
   cleanup      => true,
 }
 -> file { 'C:/install_azure_agent.ps1':
@@ -16,9 +16,9 @@ file { 'C:/tools/vsts-agent-win-x86-2.144.2':
   mode    => '0644',
   content => @(EOT)
     Write-Host "Configuring Azure DevOps agent"
-    C:\tools\vsts-agent-win-x86-2.144.2\config.cmd --unattended --url http://localhost:9090 --auth integrated --pool default --agent myAgent
+    C:\tools\vsts-agent\config.cmd --unattended --url http://localhost:9090 --auth integrated --pool default --agent myAgent
     Write-Host "Starting Azure DevOps agent"
-    start "C:\tools\vsts-agent-win-x86-2.144.2\run.cmd" -PassThru
+    start "C:\tools\vsts-agent\run.cmd" -PassThru
     New-Item -ItemType file c:\AzureAgentStarted.txt
     exit 0
     | EOT
