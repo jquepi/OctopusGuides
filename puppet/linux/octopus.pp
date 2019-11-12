d86808560ca8a3103d0d4ff95447cbb9e4b3606c
Matthew Casperson <matthewcasperson@gmail.com>
2019-11-12 11:17:08 +1000

    Fixed up the ports

--- puppet/linux/octopus.pp
+++ puppet/linux/octopus.pp
@@ -4,8 +4,7 @@ docker::image { 'mcr.microsoft.com/mssql/server':
 -> docker::run { 'mssql':
   image => 'mcr.microsoft.com/mssql/server:2017-latest-ubuntu',
   env   => ['ACCEPT_EULA=Y', 'SA_PASSWORD=Password01!', 'MSSQL_PID=Express'],
-  ports => [1433],
-  expose => [1433]
+  ports => ['1433:1433'],
 }
 -> docker::image { 'octopusdeploy/linuxoctopus':
   image_tag => '2019.10.5'
@@ -13,8 +12,7 @@ docker::image { 'mcr.microsoft.com/mssql/server':
 -> docker::run { 'octopusdeploy':
   image => 'octopusdeploy/linuxoctopus:2019.10.5',
   env   => ['ADMIN_USERNAME=admin', 'ADMIN_EMAIL=octopusguides@gmail.com', 'ADMIN_PASSWORD=Password01!', 'ACCEPT_EULA=Y', 'CONNSTRING=Server=host.docker.internal,1433;Database=Octopus;User Id=SA;Password=Password01!'],
-  ports => [80],
-  expose => [8080]
+  ports => ['80:8080'],
 }
 -> file { '/opt/octo':
   ensure => 'directory'
