968578d9556438ed05024bd90526af162133e716
Matthew Casperson <matthewcasperson@gmail.com>
2019-11-12 18:57:34 +1000

    Moving code to shared scripts

---
+++ scripts/create-certificates.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+/usr/lib/jvm/java-13-openjdk-amd64/bin/java \
+  "--enable-preview" \
+  "-Xmx2g" \
+  "-Dwebdriver.gecko.driver=/usr/bin/geckodriver" \
+  "-DCucumberAlias-GuideSpecificScreenshotDir=$SCREENSHOT_PATH" \
+  "-DmoveCursorToElement=$MOVE_TO_MOUSE_CURSOR" \
+  "-DmouseMoveVerticalOffset=$MOUSE_VERTICAL_OFFSET" \
+  "-DdisableHighlights=$DISABLE_HIGHLIGHTS" \
+  "-DdisableScreenshots=$DISABLE_SCREENSHOTS" \
+  "-DslackHookUrl=${{ secrets.SLACK_HOOK_URL }}" \
+  "-DscreenshotS3Enabled=$STHREE_SCREENSHOT_ENABLED" \
+  "-DscreenshotS3Bucket=$SCREENSHOT_BUCKET" \
+  "-DstepHandlerMessage=$GITHUB_SHA" \
+  "-DslackStepHandlerEnabled=true" \
+  -jar /opt/webdrivertraining-1.0-SNAPSHOT.jar \
+  --plugin progress \
+  --monochrome \
+  features/octopus/octopus-create-certificate.feature
