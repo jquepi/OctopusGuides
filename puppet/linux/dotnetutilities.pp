8b5c10bfa679e59f03eded61e47160053ddec005
Matthew Casperson <matthewcasperson@gmail.com>
2019-11-12 11:30:39 +1000

    Fixing up networking

--- puppet/linux/dotnetutilities.pp
+++ puppet/linux/dotnetutilities.pp
@@ -1,7 +1,11 @@
-package { 'packages-microsoft-prod':
+file { '/opt/packages-microsoft-prod.deb':
+  source => 'https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb',
+  mode   => '0644'
+}
+-> package { 'packages-microsoft-prod':
   ensure   => installed,
   provider => dpkg,
-  source   => 'https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb'
+  source   => '/opt/packages-microsoft-prod.deb'
 }
 -> package { 'apt-transport-https':
   ensure => installed,
--- puppet/linux/octopus.pp
+++ puppet/linux/octopus.pp
@@ -1,18 +1,28 @@
-docker::image { 'mcr.microsoft.com/mssql/server':
+docker_network { 'octopus':
+  ensure   => present,
+  driver   => 'overlay',
+  subnet   => '192.168.1.0/24',
+  gateway  => '192.168.1.1',
+  ip_range => '192.168.1.4/32',
+}
+-> docker::image { 'mcr.microsoft.com/mssql/server':
   image_tag => '2017-latest-ubuntu'
 }
 -> docker::run { 'mssql':
   image => 'mcr.microsoft.com/mssql/server:2017-latest-ubuntu',
   env   => ['ACCEPT_EULA=Y', 'SA_PASSWORD=Password01!', 'MSSQL_PID=Express'],
   ports => ['1433:1433'],
+  net   => 'octopus'
 }
 -> docker::image { 'octopusdeploy/linuxoctopus':
   image_tag => '2019.10.5'
 }
 -> docker::run { 'octopusdeploy':
   image => 'octopusdeploy/linuxoctopus:2019.10.5',
-  env   => ['ADMIN_USERNAME=admin', 'ADMIN_EMAIL=octopusguides@gmail.com', 'ADMIN_PASSWORD=Password01!', 'ACCEPT_EULA=Y', 'CONNSTRING=Server=host.docker.internal,1433;Database=Octopus;User Id=SA;Password=Password01!'],
+  env   => ['ADMIN_USERNAME=admin', 'ADMIN_EMAIL=octopusguides@gmail.com', 'ADMIN_PASSWORD=Password01!', 'ACCEPT_EULA=Y'
+    , 'CONNSTRING=Server=mssql,1433;Database=Octopus;User Id=SA;Password=Password01!'],
   ports => ['80:8080'],
+  net   => 'octopus'
 }
 -> file { '/opt/octo':
   ensure => 'directory'
