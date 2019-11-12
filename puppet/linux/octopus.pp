bd9a0934da7d40a7ebc58b04fa2622103255aa11
Matthew Casperson <matthewcasperson@gmail.com>
2019-11-12 10:48:00 +1000

    Testing octopus has started

--- puppet/linux/octopus.pp
+++ puppet/linux/octopus.pp
@@ -19,11 +19,23 @@ docker::image { 'mcr.microsoft.com/mssql/server':
 -> file { '/opt/octo':
   ensure => 'directory'
 }
--> archive { '/opt/OctopusTools.6.14.2.portable.zip':
+-> archive { '/opt/OctopusTools.6.14.2.portable.tar.gz':
   ensure       => present,
   extract      => true,
   extract_path => '/opt/octo',
-  source       => 'https://download.octopusdeploy.com/octopus-tools/6.14.2/OctopusTools.6.14.2.portable.zip',
+  source       => 'https://download.octopusdeploy.com/octopus-tools/6.14.2/OctopusTools.6.14.2.portable.tar.gz',
   creates      => '/opt/octo/Octo',
   cleanup      => true,
+}
+-> file { '/opt/query-octopus.sh':
+  ensure  => 'file',
+  mode    => '0744',
+  content => @(EOT)
+    #!/bin/bash
+    /opt/octo/octo list-machines --server http://localhost --user admin --pass Password01!
+    | EOT
+}
+-> exec { 'Query Octopus':
+  command   => '/opt/query-octopus.sh',
+  logoutput => true
 }
