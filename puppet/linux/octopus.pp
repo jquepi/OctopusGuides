54f7fb5012df513a468925c8bb0d15c122584d56
Matthew Casperson <matthewcasperson@gmail.com>
2019-11-13 14:02:07 +1000

    Working on saving api key

--- puppet/linux/octopus.pp
+++ puppet/linux/octopus.pp
@@ -48,34 +48,3 @@ docker_network { 'octopus':
   command   => '/opt/query-octopus.sh',
   logoutput => true
 }
--> file { '/opt/installtentacle.sh':
-  ensure  => 'file',
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0755',
-  content => @(EOT)
-serverUrl="http://localhost"      # The url of your Octous server
-serverCommsPort=10943             # The communication port the Octopus Server is listening on (10943 by default)
-apiKey=""                         # An Octopus Server api key with permission to add machines
-name=$HOSTNAME                    # The name of the Tentacle at is will appear in the Octopus portal
-workerPool="Default Worker Pool"  # The worker pool to register the Tentacle in
-configFilePath="/etc/octopus/default/tentacle-default.config"
-applicationPath="/home/Octopus/Applications/"
-
-apt-key adv --fetch-keys https://apt.octopus.com/public.key
-add-apt-repository "deb https://apt.octopus.com/ stretch main"
-apt-get update
-apt-get install tentacle
-
-/opt/octopus/tentacle/Tentacle create-instance --config "$configFilePath"
-/opt/octopus/tentacle/Tentacle new-certificate --if-blank
-/opt/octopus/tentacle/Tentacle configure --noListen True --reset-trust --app "$applicationPath"
-echo "Registering the Tentacle $name with server $serverUrl in environment $environment with role $role"
-/opt/octopus/tentacle/Tentacle register-worker --server "$serverUrl" --apiKey "$apiKey" --name "$name" --workerPool "$workerPool" --comms-style "TentacleActive" --server-comms-port $serverCommsPort
-/opt/octopus/tentacle/Tentacle service --install --start
-    | EOT
-}
--> exec { 'Install Octopus Tentacle':
-  command   => '/opt/installtentacle.sh',
-  logoutput => true
-}
---
+++ scripts/create-worker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+serverUrl="https://localhost"       # The url of your Octous server
+serverCommsPort=10943               # The communication port the Octopus Server is listening on (10943 by default)
+apiKey=`cat /opt/api_key.txt`       # An Octopus Server api key with permission to add machines
+name=$HOSTNAME                      # The name of the Tentacle at is will appear in the Octopus portal
+workerPool="Default Worker Pool"    # The worker pool to register the Tentacle in
+configFilePath="/etc/octopus/default/tentacle-default.config"
+applicationPath="/home/Octopus/Applications/"
+
+apt-key adv --fetch-keys https://apt.octopus.com/public.key
+add-apt-repository "deb https://apt.octopus.com/ stretch main"
+apt-get update
+apt-get install tentacle
+
+/opt/octopus/tentacle/Tentacle create-instance --config "$configFilePath"
+/opt/octopus/tentacle/Tentacle new-certificate --if-blank
+/opt/octopus/tentacle/Tentacle configure --noListen True --reset-trust --app "$applicationPath"
+echo "Registering the Tentacle $name with server $serverUrl in environment $environment with role $role"
+/opt/octopus/tentacle/Tentacle register-worker --server "$serverUrl" --apiKey "$apiKey" --name "$name" --workerPool "$workerPool" --comms-style "TentacleActive" --server-comms-port $serverCommsPort
+/opt/octopus/tentacle/Tentacle service --install --start
