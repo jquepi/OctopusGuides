aaf6453a820e1bc59641f924f16292362ac8040b
Matthew Casperson <matthewcasperson@gmail.com>
2019-11-13 15:07:29 +1000

    Have to run install as sudo

--- scripts/create-worker.sh
+++ scripts/create-worker.sh
@@ -16,6 +16,6 @@ apt-get install tentacle
 /opt/octopus/tentacle/Tentacle create-instance --config "$configFilePath"
 /opt/octopus/tentacle/Tentacle new-certificate --if-blank
 /opt/octopus/tentacle/Tentacle configure --noListen True --reset-trust --app "$applicationPath"
-echo "Registering the Tentacle $name with server $serverUrl in environment $environment with role $role"
+echo "Registering the worker $name with server $serverUrl in the worker pool $workerPool"
 /opt/octopus/tentacle/Tentacle register-worker --server "$serverUrl" --apiKey "$apiKey" --name "$name" --workerPool "$workerPool" --comms-style "TentacleActive" --server-comms-port $serverCommsPort
 /opt/octopus/tentacle/Tentacle service --install --start
