93c3bb5c9ef059184b9ad35c1bf9b8afce5b9197
Matthew Casperson <matthewcasperson@gmail.com>
2019-11-15 09:45:30 +1000

    Fixed exit code and removed verbose logging

--- scripts/convert-video.sh
+++ scripts/convert-video.sh
@@ -6,7 +6,6 @@ for f in $VIDEO_FILES
 do
   echo "Processing $f"
   vlc -I dummy \
-    -vvv \
     "$f" \
     "--sout=#transcode{vcodec=h264,vb=1024,acodec=mp4a,ab=192,channels=2,deinterlace}:standard{access=file,mux=mp4,dst=\"${f%.*}.mp4\"}" \
     vlc://quit
@@ -16,7 +15,6 @@ VIDEO_FILE_LIST=`ls $MEDIA_PATH/*.mp4 -m | tr "," " " | tr "\n" " " | tr -s " "`
 echo $VIDEO_FILE_LIST
 
 vlc -I dummy \
-  -vvv \
    $VIDEO_FILE_LIST \
    --no-sout-all \
    --sout-keep \
@@ -25,3 +23,5 @@ vlc -I dummy \
    vlc://quit
 
 ffmpeg -i $MEDIA_PATH/video.mp4 "-filter:v" "setpts=0.75*PTS" "$MEDIA_PATH/fast.mp4"
+
+exit 0
