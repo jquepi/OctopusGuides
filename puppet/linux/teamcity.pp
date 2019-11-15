79cc903c68a7d9b060c8445967bdc3b21da058ef
Matthew Casperson <matthewcasperson@gmail.com>
2019-11-15 15:16:46 +1000

    Setting up team city

---
+++ puppet/linux/teamcity.pp
@@ -0,0 +1,26 @@
+archive { '/opt/TeamCity-2019.1.4.tar.gz':
+  ensure          => present,
+  extract         => true,
+  extract_path    => 'C:/opt',
+  source          => 'https://octopus-guides.s3.amazonaws.com/teamcity/TeamCity-2019.1.4.tar.gz',
+  creates         => 'C:/opt/TeamCity/TeamCity-readme.txt',
+  cleanup         => true,
+  extract_command => 'tar xfz %s'
+}
+-> file { '/opt/start_teamcity.sh':
+  ensure  => 'file',
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0755',
+  content => @(EOT)
+    /opt/TeamCity\bin/runAll.sh start &
+    touch /opt/TeamCityStarted.txt
+    exit 0
+    | EOT
+}
+-> exec { 'Start TeamCity':
+  command   => '/opt/start_teamcity.sh',
+  creates   => '/opt/TeamCityStarted.txt',
+  provider  => powershell,
+  logoutput => true
+}
