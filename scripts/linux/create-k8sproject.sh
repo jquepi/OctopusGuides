b1d0a88f92d9beb84e8d66044392832103feb5db
Matthew Casperson <matthewcasperson@gmail.com>
2019-11-19 09:55:47 +1000

    Fixed up passing in tags

--- scripts/linux/create-k8sproject.sh
+++ scripts/linux/create-k8sproject.sh
@@ -20,7 +20,7 @@
   "-DstepHandlerMessage=$GITHUB_SHA" \
   "-DslackStepHandlerEnabled=true" \
   -jar /opt/webdrivertraining-1.0-SNAPSHOT.jar \
-  --tags $CUCUMBER_TAGS \
+  --tags "$CUCUMBER_TAGS" \
   --plugin progress \
   --monochrome \
   features/octopus/octopus-k8s-project.feature
