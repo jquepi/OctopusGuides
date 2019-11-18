8b325d2a70c401a07a5ff9324e00c371317c8e6d
Matthew Casperson <matthewcasperson@gmail.com>
2019-11-19 09:09:38 +1000

    Fixed up passing tags in

--- scripts/linux/create-teamcitycreaterelease.sh
+++ scripts/linux/create-teamcitycreaterelease.sh
@@ -23,6 +23,7 @@ API_KEY=`cat /tmp/api_key.txt`
   "-DCucumberAlias-ExternalDockerUsername=$DOCKER_USERNAME" \
   "-DCucumberAlias-ExternalDockerPassword=$DOCKER_PASSWORD" \
   -jar /opt/webdrivertraining-1.0-SNAPSHOT.jar \
+  --tags $CUCUMBER_TAGS \
   --plugin progress \
   --monochrome \
   features/teamcity/teamcity-create-deploy.feature
