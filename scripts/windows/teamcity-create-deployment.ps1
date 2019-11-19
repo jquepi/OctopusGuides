f122a2ce3cfd459bd411f4f39db34b5c4be51401
Matthew Casperson <matthewcasperson@gmail.com>
2019-11-20 06:19:11 +1000

    Added slack logging

--- scripts/windows/teamcity-create-deployment.ps1
+++ scripts/windows/teamcity-create-deployment.ps1
@@ -15,6 +15,7 @@
     "-DstepHandlerMessage=$env:GITHUB_SHA" `
     "-DCucumberAlias-ExternalMediaPath=$env:MEDIA_PATH" `
     "-DCucumberAlias-ExternalBrowserType=FirefoxNoImplicitWait" `
+    "-DCucumberAlias-ExternalOctopusAPIKey=$((cat C:\octopus_api.txt).Trim())" `
     -jar c:\tools\webdrivertraining-1.0-SNAPSHOT.jar `
     --tags $env:CUCUMBER_TAGS `
     --plugin progress `
