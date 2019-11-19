d31d43cebe63ffa90fec2dae350e830b07bc6eeb
Matthew Casperson <matthewcasperson@gmail.com>
2019-11-20 07:50:27 +1000

    Added slack logging

--- scripts/windows/configure-artifactory.ps1
+++ scripts/windows/configure-artifactory.ps1
@@ -2,17 +2,17 @@
     "--enable-preview" `
     "-Xmx2g" `
     "-Dwebdriver.gecko.driver=C:\tools\geckodriver.exe" `
-    "-DCucumberAlias-ExternalArtifactoryKey=$env:ARTIFACTORY_KEY" `
     "-DmoveCursorToElement=$env:MOVE_TO_MOUSE_CUSROR" `
     "-DmouseMoveVerticalOffset=$env:MOUSE_VERTICAL_OFFSET" `
     "-DdisableHighlights=$env:DISABLE_HIGHLIGHTS" `
     "-DdisableScreenshots=$env:DISABLE_SCREENSHOTS" `
     "-DdisableVideoRecording=$env:DISABLE_VIDEO" `
     "-DslackHookUrl=$env:SLACK_HOOK_URL" `
-    "-DslackStepHandlerEnabled=false" `
+    "-DslackStepHandlerEnabled=true" `
     "-DscreenshotS3Enabled=$env:STHREE_SCREENSHOT_ENABLED" `
     "-DscreenshotS3Bucket=$env:SCREENSHOT_BUCKET" `
     "-DstepHandlerMessage=$env:GITHUB_SHA" `
+    "-DCucumberAlias-ExternalArtifactoryKey=$env:ARTIFACTORY_KEY" `
     "-DCucumberAlias-ExternalMediaPath=$env:MEDIA_PATH" `
     "-DCucumberAlias-ExternalBrowserType=FirefoxNoImplicitWait" `
     -jar c:\tools\webdrivertraining-1.0-SNAPSHOT.jar `
