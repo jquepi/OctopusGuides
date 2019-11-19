803a0a48a1042329553777998f7ecf180786bb0f
Matthew Casperson <matthewcasperson@gmail.com>
2019-11-19 14:13:24 +1000

    Cleaned up more jenkins workflows

---
+++ scripts/windows/create-externalfeed.ps1
@@ -0,0 +1,21 @@
+& "C:\Program Files\OpenJDK\jdk-13\bin\java" `
+    "--enable-preview" `
+    "-Xmx2g" `
+    "-Dwebdriver.gecko.driver=C:\tools\geckodriver.exe" `
+    "-DCucumberAlias-ExternalArtifactoryKey=$env:ARTIFACTORY_KEY" `
+    "-DmoveCursorToElement=$env:MOVE_TO_MOUSE_CUSROR" `
+    "-DmouseMoveVerticalOffset=$env:MOUSE_VERTICAL_OFFSET" `
+    "-DdisableHighlights=$env:DISABLE_HIGHLIGHTS" `
+    "-DdisableScreenshots=$env:DISABLE_SCREENSHOTS" `
+    "-DdisableVideoRecording=$env:DISABLE_VIDEO" `
+    "-DslackHookUrl=$env:SLACK_HOOK_URL" `
+    "-DslackStepHandlerEnabled=false" `
+    "-DscreenshotS3Enabled=$env:STHREE_SCREENSHOT_ENABLED" `
+    "-DscreenshotS3Bucket=$env:SCREENSHOT_BUCKET" `
+    "-DstepHandlerMessage=$env:GITHUB_SHA" `
+    "-DCucumberAlias-ExternalMediaPath=$env:MEDIA_PATH" `
+    "-DCucumberAlias-ExternalOctopusAPIKey=$((cat C:\octopus_api.txt).Trim())" `
+    -jar c:\tools\webdrivertraining-1.0-SNAPSHOT.jar `
+    --plugin progress `
+    --monochrome `
+    features\octopus\octopus-artifactory-feed.feature
