1ecdcdd89d8ac539fc9bd8b517c5dcef8d6737e1
Matthew Casperson <matthewcasperson@gmail.com>
2019-11-25 15:15:24 +1000

    Fixed script

--- checkpages.ps1
+++ checkpages.ps1
@@ -1,22 +1,21 @@
 $errorPages = @()
 
 foreach($line in Get-Content .\links.txt) {
-    if($line -match $regex){
-        Write-Host "Testing $line"
+    Write-Host "Testing $line"
 
-        & "C:\Program Files\Java\jdk-13\bin\java.exe" `
-        --enable-preview `
-        "-Dio.netty.leakDetection.level=DISABLED" `
-        "-Dwebdriver.gecko.driver=c:\tools\geckodriver.exe" `
-        "-DCucumberAlias-ExternalBrowserType=FirefoxHeadless" `
-        "-DCucumberAlias-ExternalURL=$line" `
-        -jar ..\WebDriverTraining\target\webdrivertraining-1.0-SNAPSHOT.jar `
-        features\verify\verify.feature
+    & "C:\Program Files\Java\jdk-13\bin\java.exe" `
+    --enable-preview `
+    "-Dio.netty.leakDetection.level=DISABLED" `
+    "-Dwebdriver.gecko.driver=c:\tools\geckodriver.exe" `
+    "-DCucumberAlias-ExternalBrowserType=FirefoxHeadless" `
+    "-DCucumberAlias-ExternalURL=$line" `
+    -jar ..\WebDriverTraining\target\webdrivertraining-1.0-SNAPSHOT.jar `
+    features\verify\verify.feature
 
-        if ($LASTEXITCODE -ne 0) {
-            $errorPages += $line
-        }
+    if ($LASTEXITCODE -ne 0) {
+        $errorPages += $line
     }
+
     #break
 }
 
