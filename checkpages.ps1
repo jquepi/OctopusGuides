0edc161fffd3eebbb1db5f3ab4ec50b4ac989342
Matthew Casperson <matthewcasperson@gmail.com>
2019-11-25 14:23:02 +1000

    Fixed some error messages

--- checkpages.ps1
+++ checkpages.ps1
@@ -1,13 +1,27 @@
+$errorPages = @()
+
 foreach($line in Get-Content .\links.txt) {
     if($line -match $regex){
         Write-Host "Testing $line"
+
         & "C:\Program Files\Java\jdk-13\bin\java.exe" `
         --enable-preview `
+        "-Dio.netty.leakDetection.level=DISABLED" `
         "-Dwebdriver.gecko.driver=c:\tools\geckodriver.exe" `
         "-DCucumberAlias-ExternalBrowserType=FirefoxHeadless" `
         "-DCucumberAlias-ExternalURL=$line" `
         -jar ..\WebDriverTraining\target\webdrivertraining-1.0-SNAPSHOT.jar `
         features\verify\verify.feature
+
+        if ($LASTEXITCODE -ne 0) {
+            $errorPages += $line
+        }
     }
     #break
+}
+
+if ($errorPages.Count -ne 0)
+{
+    Write-Host "Found errors:"
+    $errorPages | % { Write-Host $_ }
 }
