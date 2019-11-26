11a9560c84c814f09050f9a1fa904973149ce962
Matthew Casperson <matthewcasperson@gmail.com>
2019-11-26 14:08:53 +1000

    Started work on Bamboo K8S guide

---
+++ puppet/linux/bamboo.pp
@@ -0,0 +1,40 @@
+apt::key { 'atlassian-repository':
+  id     => 'D648E5571A519459EC58F888B56C9565957495B9',
+  source => 'https://packages.atlassian.com/api/gpg/key/public',
+  server  => 'pgp.mit.edu'
+}
+-> apt::source { 'atlassian':
+  comment  => 'This is the atlassian repository',
+  location => 'https://packages.atlassian.com/debian/atlassian-sdk-deb/',
+  release  => '',
+  repos    => 'stable contrib',
+  key      => {
+    'id' => 'D648E5571A519459EC58F888B56C9565957495B9',
+  },
+  include  => {
+    'deb' => true,
+  },
+}
+-> package { 'atlassian-plugin-sdk':
+  ensure => installed,
+}
+-> file { '/opt/start_bamboo.sh':
+  ensure  => 'file',
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0755',
+  content => @(EOT)
+    #!/bin/bash
+    mkdir -p /tmp/bamboo/target/classes
+    cp config/pom.xml /tmp/bamboo
+    cp config/atlassian-plugin.xml /tmp/bamboo/target/classes
+    cd /tmp/bamboo
+    atlas-run &
+    touch /opt/BambooStarted.txt
+    | EOT
+}
+-> exec { 'Start Bamboo':
+  command   => '/opt/start_bamboo.sh',
+  creates   => '/opt/BambooStarted.txt',
+  logoutput => true
+}
