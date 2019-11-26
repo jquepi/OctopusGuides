2ac07dfcc7d64cee71eaa6b1f48009945d8d1380
Matthew Casperson <matthewcasperson@gmail.com>
2019-11-26 15:58:50 +1000

    Added a wait for the bamboo homepage

--- puppet/linux/bamboofinalize.pp
+++ puppet/linux/bamboofinalize.pp
@@ -1,3 +1,3 @@
 exec {"wait for bamboo":
-  command => "max=30; while ! wget --spider http://localhost:6990/bamboo > /dev/null 2>&1; do max=$(( max - 1 )); [ $max -lt 0 ] && break; echo 'Sleeping';  sleep 10; done;",
+  command => "max=30; while [[ \"$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:6990/bamboo/)\" != \"200\" ]]; do max=$(( max - 1 )); [ $max -lt 0 ] && break; echo 'Sleeping';  sleep 10; done;",
 }
