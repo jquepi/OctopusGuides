4a76ef5c11a972b2f0a79254a395d8104a1fa842
Matthew Casperson <matthewcasperson@gmail.com>
2019-11-26 19:08:20 +1000

    Fixed execution of wait script

--- puppet/linux/bamboofinalize.pp
+++ puppet/linux/bamboofinalize.pp
@@ -1,3 +1,14 @@
-exec {"wait for bamboo":
-  command => "max=30; while [[ \"$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:6990/bamboo/)\" != \"200\" ]]; do max=$(( max - 1 )); [ $max -lt 0 ] && break; echo 'Sleeping';  sleep 10; done;",
+file { '/opt/wait_for_bamboo.sh':
+  ensure  => 'file',
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0755',
+  content => @(EOT)
+    #!/bin/bash
+    max=60; while [[ \"$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:6990/bamboo/)\" != \"200\" ]]; do max=$(( max - 1 )); [ $max -lt 0 ] && break; echo 'Sleeping';  sleep 10; done;
+    exit 0
+    | EOT
+}
+-> exec {"wait for bamboo":
+  command => "/opt/wait_for_bamboo.sh",
 }
