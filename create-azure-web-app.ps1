205c522263c5c50b91e4046db7c4138c36b8cc38
Matthew Casperson <matthewcasperson@gmail.com>
2019-11-30 20:40:45 +1000

    Deleting resource groups before recreating

--- create-azure-web-app.ps1
+++ create-azure-web-app.ps1
@@ -3,28 +3,12 @@ param(
 )
 
 $exists = & az group exists --name $name
-if ($exists -eq "false") {
-    Write-Host "Resource group $name does not exist. Creating..."
-    az group create --location australiaeast --name $name --tags Lifetime=Permanent OwnerContact=@matthew.casperson
-} else {
-    Write-Host "Resource group $name already exists."
+if ($exists -eq "true") {
+    Write-Host "Resource group $name already exists. Deleting."
+    az group delete --name $name --yes
 }
 
-$plans = & az appservice plan list --resource-group $name | ConvertFrom-Json
-if ($plans.Count -eq 0) {
-    Write-Host "App service plan $name does not exist. Creating..."
-    az appservice plan create --name $name --resource-group $name --number-of-workers 1 --sku B1
-
-}
-else {
-    Write-Host "Resource group $name already exists."
-}
-
-$apps = az webapp list --resource-group $name | ConvertFrom-Json
-if ($apps.Count -eq 0) {
-    Write-Host "Webapp $name does not exist. Creating..."
-    az webapp create -g $name -p $name -n $name --% --runtime "aspnet|V4.7"
-    az webapp log config -g $name -n $name --application-logging true --detailed-error-messages true --web-server-logging filesystem
-} else {
-    Write-Host "Webapp $name already exists."
-}
+az group create --location australiaeast --name $name --tags Lifetime=Permanent OwnerContact=@matthew.casperson
+az appservice plan create --name $name --resource-group $name --number-of-workers 1 --sku B1
+az webapp create -g $name -p $name -n $name --% --runtime "aspnet|V4.7"
+az webapp log config -g $name -n $name --application-logging true --detailed-error-messages true --web-server-logging filesystem
