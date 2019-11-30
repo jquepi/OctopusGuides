483a43b7a8c3efee7b0fb78db46f3f27c7000bf3
Matthew Casperson <matthewcasperson@gmail.com>
2019-11-30 20:37:02 +1000

    Deleting resource groups before starting

--- delete-resource-group.ps1
+++ delete-resource-group.ps1
@@ -1,5 +1,11 @@
-param([String] $name)
+param([String] $name, [switch] $Wait = $false)
 $exists = & az group exists --name $name
 if ($exists -eq "true") {
-    az group delete --name $name --yes --no-wait
+    if ($Wait)
+    {
+        az group delete --name $name --yes
+    }
+    else {
+        az group delete --name $name --yes --no-wait
+    }
 }
