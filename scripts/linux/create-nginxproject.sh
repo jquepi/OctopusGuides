73b26b95320a6ffeceefc77306f397eb6cf0dd7a
Matthew Casperson <matthewcasperson@gmail.com>
2019-12-12 06:37:29 +1000

    Added a nodejs nginx jenkins workflow

---
+++ scripts/linux/create-nginxproject.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+/usr/lib/jvm/java-13-openjdk-amd64/bin/java \
+  "--enable-preview" \
+  "-Xmx2g" \
+  "-Dwebdriver.gecko.driver=/usr/bin/geckodriver" \
+  "-DCucumberAlias-GuideSpecificScreenshotDir=$SCREENSHOT_PATH" \
+  "-DCucumberAlias-ExternalMediaPath=$MEDIA_PATH" \
+  "-DmoveCursorToElement=$MOVE_TO_MOUSE_CURSOR" \
+  "-DmouseMoveVerticalOffset=$MOUSE_VERTICAL_OFFSET_CHROME" \
+  "-DdisableHighlights=$DISABLE_HIGHLIGHTS" \
+  "-DdisableScreenshots=$DISABLE_SCREENSHOTS" \
+  "-DdisableVideoRecording=$DISABLE_VIDEO" \
+  "-DslackHookUrl=$SLACK_HOOK_URL" \
+  "-DscreenshotS3Enabled=$STHREE_SCREENSHOT_ENABLED" \
+  "-DscreenshotS3Bucket=$SCREENSHOT_BUCKET" \
+  "-DstepHandlerMessage=$GITHUB_SHA" \
+  "-DslackStepHandlerEnabled=true" \
+  "-DCucumberAlias-ExternalBrowserType=ChromeNoImplicitWait" \
+  "-DCucumberAlias-ExternalDockerImage=$DOCKER_IMAGE" \
+  "-DCucumberAlias-ExternalConfigFile=$CONFIG_FILE" \
+  "-DCucumberAlias-ExternalConfigMountPath=$CONFIG_FILE_MOUNT_PATH" \
+  "-DCucumberAlias-ExternalConfigFileName=$CONFIG_FILE_NAME" \
+  -jar /opt/webdrivertraining.1.0-SNAPSHOT.jar \
+  --tags "$CUCUMBER_TAGS" \
+  --plugin progress \
+  features/octopus/octopus-nginx-project.feature
---
+++ scripts/linux/open-devnginxapp.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+MINIKUBE_IP=`cat /tmp/minikubeip.txt`
+echo "Minikube IP: $MINIKUBE_IP"
+
+/usr/lib/jvm/java-13-openjdk-amd64/bin/java \
+  "--enable-preview" \
+  "-Xmx2g" \
+  "-Dwebdriver.gecko.driver=/usr/bin/geckodriver" \
+  "-DCucumberAlias-GuideSpecificScreenshotDir=$SCREENSHOT_PATH" \
+  "-DCucumberAlias-ExternalMediaPath=$MEDIA_PATH" \
+  "-DmoveCursorToElement=$MOVE_TO_MOUSE_CURSOR" \
+  "-DmouseMoveVerticalOffset=$MOUSE_VERTICAL_OFFSET_CHROME" \
+  "-DdisableHighlights=$DISABLE_HIGHLIGHTS" \
+  "-DdisableScreenshots=$DISABLE_SCREENSHOTS" \
+  "-DdisableVideoRecording=$DISABLE_VIDEO" \
+  "-DslackHookUrl=$SLACK_HOOK_URL" \
+  "-DscreenshotS3Enabled=$STHREE_SCREENSHOT_ENABLED" \
+  "-DscreenshotS3Bucket=$SCREENSHOT_BUCKET" \
+  "-DstepHandlerMessage=$GITHUB_SHA" \
+  "-DslackStepHandlerEnabled=true" \
+  "-DCucumberAlias-ExternalEnvironmentName=Dev" \
+  "-DCucumberAlias-ExternalTargetName=Kubernetes" \
+  "-DCucumberAlias-ExternalEnvironment=//span[./div/div/div[text()='Dev']]" \
+  "-DCucumberAlias-ExternalNamespace=randomquotes-dev" \
+  "-DCucumberAlias-ExternalUrl=http://localhost:8081" \
+  "-DCucumberAlias-Screenshot=nginx-random-quotes-dev-app.png" \
+  "-DCucumberAlias-ExternalBrowserType=ChromeNoImplicitWait" \
+  -jar /opt/webdrivertraining.1.0-SNAPSHOT.jar \
+  --plugin progress \
+  --tags "$CUCUMBER_TAGS" \
+  features/octopus/open-randomquotes-generic.feature
