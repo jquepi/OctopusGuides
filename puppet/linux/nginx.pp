4893c61679ea0b54324ca037d14a5c4e84f42ac0
Matthew Casperson <matthewcasperson@gmail.com>
2019-12-12 08:44:42 +1000

    fixed regex

--- puppet/linux/nginx.pp
+++ puppet/linux/nginx.pp
@@ -4,20 +4,20 @@ package { 'nginx':
 -> file_line { 'Add additional config locations':
   path    => '/etc/nginx/nginx.conf',
   line    => '    include /etc/nginx/conf.d/*/*.conf;',
-  after   => '^\w+include /etc/nginx/conf.d/*.conf;',
+  after   => '^\s+include /etc/nginx/conf.d/*.conf;',
   notify  => Service['nginx']
 }
 -> file_line { 'Change default ipv4 port':
   path    => '/etc/nginx/sites-enabled/default',
   line    => '    listen 4444;',
-  match   => '^\w*listen 80;',
+  match   => '^\s*listen 80;',
   replace => true,
   notify  => Service['nginx']
 }
 -> file_line { 'Change default ipv6 port':
   path    => '/etc/nginx/sites-enabled/default',
   line    => '    listen [::]:4444;',
-  match   => '^\w*listen [::]:80;',
+  match   => '^\s*listen [::]:80;',
   replace => true,
   notify  => Service['nginx']
 }
