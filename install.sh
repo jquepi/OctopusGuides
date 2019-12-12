811571e2743fcefa1c01b097fa3b1add3c5bc877
Matthew Casperson <matthewcasperson@gmail.com>
2019-12-12 19:20:45 +1000

    Still trying to work out the initial config

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# https://stackoverflow.com/questions/59895/get-the-source-directory-of-a-bash-script-from-within-the-script-itself
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 wget https://apt.puppetlabs.com/puppet6-release-bionic.deb
