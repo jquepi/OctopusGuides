a82f219a1f74a67729cb806d9c2ff3271b72b2f1
Matthew Casperson <matthewcasperson@gmail.com>
2019-12-13 09:19:55 +1000

    Moved config file updating to later in the process

--- puppet/linux/jenkins.pp
+++ puppet/linux/jenkins.pp
@@ -158,21 +158,3 @@ file { '/var/lib/jenkins':
   group => "docker",
   mode => '0777',
 }
--> file_line { 'jenkins args':
-  path    => '/etc/default/jenkins',
-  line    => 'JENKINS_ARGS="--webroot=/var/cache/$NAME/war --httpPort=$HTTP_PORT" --argumentsRealm.passwd.admin=Password01! --argumentsRealm.roles.admin=admin',
-  match   => 'JENKINS_ARGS="--webroot=/var/cache/\$NAME/war --httpPort=\$HTTP_PORT"',
-  replace => true
-}
--> file_line { 'Use container security':
-  path    => '/var/lib/jenkins/config.xml',
-  line    => '  <authorizationStrategy class="hudson.security.LegacyAuthorizationStrategy"/>',
-  match   => '^\s*<authorizationStrategy.*?/>',
-  replace => true
-}
--> file_line { 'Legacy realm':
-  path    => '/var/lib/jenkins/config.xml',
-  line    => '  <securityRealm class="hudson.security.LegacySecurityRealm"/>',
-  match   => '^\s*<securityRealm.*?/>',
-  replace => true
-}
