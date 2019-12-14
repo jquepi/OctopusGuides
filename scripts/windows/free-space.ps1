6882274732ba2d2d4a066cea07cf3192345c8bf0
Matthew Casperson <matthewcasperson@gmail.com>
2019-12-15 07:35:09 +1000

    The two step approach to disk compression seems to work

--- scripts/windows/free-space.ps1
+++ scripts/windows/free-space.ps1
@@ -1,12 +1,21 @@
 # Some Azure Devops builds run out of space, so compress the drive to give us enough space.
+# This script is run before the apps are installed, and then again after
 
 $ErrorActionPreference = "SilentlyContinue"
 
-mkdir "C:\Octopus"
-mkdir "C:\Program Files\Azure DevOps Server 2019"
-
-cd "C:\Program Files"
+# This gives us enough space to complete the install
+cd C:\Windows
 compact /c /s /i
 
-cd C:\Octopus
-compact /c /s /i
+# This gives us enough space to perform the deployments
+if ( Test-Path -Path 'C:\Octopus' -PathType Container )
+{
+    cd C:\Octopus
+    compact /c /s /i
+}
+
+if ( Test-Path -Path "C:\Program Files\Azure DevOps Server 2019" -PathType Container )
+{
+    cd "C:\Program Files\Azure DevOps Server 2019"
+    compact /c /s /i
+}
