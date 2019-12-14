1bbd2cf38db3d2be292f0597e23d885e2b489f71
Matthew Casperson <matthewcasperson@gmail.com>
2019-12-14 17:29:41 +1000

    Need to be a bit more selective with the compressed fies

--- scripts/windows/free-space.ps1
+++ scripts/windows/free-space.ps1
@@ -1,3 +1,9 @@
 # Some Azure Devops builds run out of space, so compress the drive to give us enough space.
-cd C:\
+cd C:\Windows
+compact /c /s /i
+
+cd C:\Octopus
+compact /c /s /i
+
+cd "C:\Program Files\Azure DevOps Server 2019"
 compact /c /s /i
