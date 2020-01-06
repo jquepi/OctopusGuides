d63fbd65a6c6315a3167f8a2ea93daf353f86628
Matthew Casperson <matthewcasperson@gmail.com>
2020-01-06 10:44:31 +1000

    Fixed a alias

--- scripts/linux/create-javawar-bambooproject.sh
+++ scripts/linux/create-javawar-bambooproject.sh
@@ -24,6 +24,7 @@ API_KEY=`cat /tmp/api_key.txt`
   "-DCucumberAlias-ExternalDockerPassword=$DOCKER_PASSWORD" \
   "-DCucumberAlias-ArtifactPath=target/randomquotes.1.0.\${bamboo.buildNumber}.war" \
   "-DCucumberAlias-ExternalBrowserType=ChromeNoImplicitWait" \
+  "-DCucumberAlias-Hostname=$(hostname)" \
   -jar /opt/webdrivertraining.1.0-SNAPSHOT.jar \
   --tags "$CUCUMBER_TAGS" \
   --plugin progress \
