d591a47f77d0ae3e661df15c6dafe05afb85373e
Matthew Casperson <matthewcasperson@gmail.com>
2020-01-10 07:29:27 +1000

    Added slack logging to find issue with manual interventions

--- scripts/linux/add-manualintervention.sh
+++ scripts/linux/add-manualintervention.sh
@@ -15,6 +15,7 @@
   "-DscreenshotS3Enabled=$STHREE_SCREENSHOT_ENABLED" \
   "-DscreenshotS3Bucket=$SCREENSHOT_BUCKET" \
   "-DstepHandlerMessage=$GITHUB_SHA" \
+  "-DslackStepHandlerEnabled=true" \
   "-DCucumberAlias-ExternalBrowserType=ChromeNoImplicitWait" \
   -jar /opt/webdrivertraining.1.0-SNAPSHOT.jar \
   --plugin progress \
