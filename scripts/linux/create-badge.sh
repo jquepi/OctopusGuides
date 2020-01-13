8aab1e972ae1bd7c2692b9aaf3ab3204e3614919
Matthew Casperson <matthewcasperson@gmail.com>
2020-01-13 16:01:41 +1000

    Fixed sudo command

--- scripts/linux/create-badge.sh
+++ scripts/linux/create-badge.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-npm install -g gh-badges
+sudo npm install -g gh-badges
 mkdir "$MEDIA_PATH/badges"
 badge verified "$(date "+%d %b %y")" :green .svg @flat > "$MEDIA_PATH/badges/$BUILD_NAME.svg"
 aws s3 cp "$MEDIA_PATH/badges/$BUILD_NAME.svg" s3://i.octopus.com/guides/badges/ --acl public-read
