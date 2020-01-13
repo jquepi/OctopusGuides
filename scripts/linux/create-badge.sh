e9e942781197d4db2372176de19943cf1227044d
Matthew Casperson <matthewcasperson@gmail.com>
2020-01-14 07:11:19 +1000

    Fixing a disk space issue

--- scripts/linux/create-badge.sh
+++ scripts/linux/create-badge.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Generate a badge showing when the last video build was successfully run. This badge is included in the guides
+# with a link to the blog post https://octopus.com/blog/devops-documentation
+# We use https://npm.runkit.com/gh-badges to generatethe badge image
+
 sudo npm install -g gh-badges
 mkdir "$MEDIA_PATH/badges"
 badge verified "$(date "+%d %b %y")" :green .svg @flat > "$MEDIA_PATH/badges/$BUILD_NAME.svg"
