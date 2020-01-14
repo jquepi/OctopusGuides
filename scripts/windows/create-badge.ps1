85f2c4e00fc897b16af6937a069e7abcd5cd4561
Matthew Casperson <matthewcasperson@gmail.com>
2020-01-14 19:26:22 +1000

    Fixed the mkdir statement

--- scripts/windows/create-badge.ps1
+++ scripts/windows/create-badge.ps1
@@ -1,4 +1,4 @@
 npm install -g gh-badges
-mkdir "$MEDIA_PATH\badges"
+mkdir "$($env:MEDIA_PATH)\badges"
 get-date -format "dd MMM yyyy" ":green" ".svg" "@flat" > "$($env:MEDIA_PATH)\badges\$($env:BUILD_NAME).svg"
 & "C:\Program Files\Amazon\AWSCLI\bin\aws.exe" s3 cp "$($env:MEDIA_PATH)\badges\$($env:BUILD_NAME).svg" s3://i.octopus.com/guides/badges/ --acl public-read
