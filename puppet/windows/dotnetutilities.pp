ffaf8393ae6bc7c253d815087ee0b52121c4ddf1
Matthew Casperson <matthewcasperson@gmail.com>
2020-01-14 11:22:08 +1000

    Might need to manually set nuget repo

--- puppet/windows/dotnetutilities.pp
+++ puppet/windows/dotnetutilities.pp
@@ -37,3 +37,20 @@ package { 'NuGet.CommandLine':
   ensure   => installed,
   provider => chocolatey
 }
+-> file { 'C:/add_nuget_repo.ps1':
+  ensure  => 'file',
+  owner   => 'Administrators',
+  group   => 'Administrators',
+  mode    => '0644',
+  content => @(EOT)
+    Start-Process "C:\ProgramData\chocolatey\bin\nuget.exe" -ArgumentList @("sources", "Add", "-Name", "Nuget", "-Source", "https://api.nuget.org/v3/index.json") -NoNewWindow -Wait
+    New-Item -ItemType file c:\NugetRepoAdded.txt
+    | EOT
+}
+-> exec { 'Install Nuget Repo':
+  command   => '& C:/add_nuget_repo.ps1',
+  creates   => 'c:/NugetRepoAdded.txt',
+  timeout   => 3600,
+  provider  => powershell,
+  logoutput => true
+}
