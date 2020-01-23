237e8ee47afdcb66bdb4e33b4ac280800ecd9b47
Matthew Casperson <matthewcasperson@gmail.com>
2020-01-23 13:40:10 +1000

    Fixed up status report

--- puppet/linux/php.pp
+++ puppet/linux/php.pp
@@ -37,6 +37,6 @@ exec { 'Show config':
   logoutput => true
 }
 exec { 'Show status':
-  command   => '/bin/systemctl status php-php-fpm.service',
+  command   => '/bin/systemctl status php-fpm.service',
   logoutput => true
 }
