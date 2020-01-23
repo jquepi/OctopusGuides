71b2df3d509d43ba7d355745ea9f7258b4b68774
Matthew Casperson <matthewcasperson@gmail.com>
2020-01-23 11:50:50 +1000

    Removed additional include statement

--- puppet/linux/nginx.pp
+++ puppet/linux/nginx.pp
@@ -1,12 +1,12 @@
 package { 'nginx':
   ensure => installed,
 }
--> file_line { 'Add additional config locations':
-  path    => '/etc/nginx/nginx.conf',
-  line    => '    include /etc/nginx/conf.d/*/*.conf;',
-  after   => '^\s*include /etc/nginx/conf.d/*.conf;',
-  notify  => Service['nginx']
-}
+# -> file_line { 'Add additional config locations':
+#   path    => '/etc/nginx/nginx.conf',
+#   line    => '    include /etc/nginx/conf.d/*/*.conf;',
+#   after   => '^\s*include /etc/nginx/conf.d/*.conf;',
+#   notify  => Service['nginx']
+# }
 -> file_line { 'Change default ipv4 port':
   path    => '/etc/nginx/sites-enabled/default',
   line    => '    listen 4444 default_server;',
