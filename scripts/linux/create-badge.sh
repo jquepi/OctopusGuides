ab61d391b6b7e42272ab8809fad1688e722e7c8e
Matthew Casperson <matthewcasperson@gmail.com>
2020-01-24 13:35:53 +1000

    Capitalised "Verified" in the badges

--- scripts/linux/create-badge.sh
+++ scripts/linux/create-badge.sh
@@ -6,5 +6,5 @@
 
 sudo npm install -g gh-badges
 mkdir "$MEDIA_PATH/badges"
-badge verified "$(date "+%d %b %y")" :green .svg @flat > "$MEDIA_PATH/badges/$BUILD_NAME.svg"
+badge Verified "$(date "+%d %b %y")" :green .svg @flat > "$MEDIA_PATH/badges/$BUILD_NAME.svg"
 aws s3 cp "$MEDIA_PATH/badges/$BUILD_NAME.svg" s3://i.octopus.com/guides/badges/ --acl public-read
