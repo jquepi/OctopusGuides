9c7d013eb8b925d4e5c6bb54319236cb04fbb0f3
Matthew Casperson <matthewcasperson@gmail.com>
2020-02-10 15:43:25 +1000

    Gave up using the built in package installer

--- puppet/linux/ruby.pp
+++ puppet/linux/ruby.pp
@@ -4,24 +4,8 @@ package { 'ruby':
 -> package { 'ruby-dev':
   ensure => 'latest'
 }
--> package { 'rspec':
-  ensure   => 'latest',
-  provider => 'gem',
-  install_options   => ['--no-document']
-}
--> package { 'sinatra':
-  ensure   => 'latest',
-  provider => 'gem',
-  install_options   => ['--no-document']
-}
--> package { 'unicorn':
-  ensure   => 'latest',
-  provider => 'gem',
-  install_options   => ['--no-document']
-}
--> package { 'bundler':
-  ensure   => 'latest',
-  provider => 'gem',
-  install_options   => ['--no-document']
+-> exec { 'Install gems':
+  command   => '/usr/bin/gem install --no-document rspec sinatra unicorn bundler',
+  logoutput => true
 }
 
