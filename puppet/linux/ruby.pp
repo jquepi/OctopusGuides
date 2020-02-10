d8e8cecbcf3e5caa5b3a0467f62299f71cb1a98e
Matthew Casperson <matthewcasperson@gmail.com>
2020-02-10 15:19:58 +1000

    Fixing gem install issue: https://github.com/puphpet/puphpet/issues/2838

--- puppet/linux/ruby.pp
+++ puppet/linux/ruby.pp
@@ -5,19 +5,23 @@ package { 'ruby':
   ensure => 'latest'
 }
 -> package { 'rspec':
-  ensure => 'latest',
-  provider => 'gem'
+  ensure   => 'latest',
+  provider => 'gem',
+  source   => 'https://rubygems.org'
 }
 -> package { 'sinatra':
-  ensure => 'latest',
-  provider => 'gem'
+  ensure   => 'latest',
+  provider => 'gem',
+  source   => 'https://rubygems.org'
 }
 -> package { 'unicorn':
-  ensure => 'latest',
-  provider => 'gem'
+  ensure   => 'latest',
+  provider => 'gem',
+  source   => 'https://rubygems.org'
 }
 -> package { 'bundler':
-  ensure => 'latest',
-  provider => 'gem'
+  ensure   => 'latest',
+  provider => 'gem',
+  source   => 'https://rubygems.org'
 }
 
