c4aae8988e4bfdb000f0005237c5e4fc01ed8143
Matthew Casperson <matthewcasperson@gmail.com>
2020-02-10 15:29:41 +1000

    Trying to fix the options

--- puppet/linux/ruby.pp
+++ puppet/linux/ruby.pp
@@ -7,21 +7,21 @@ package { 'ruby':
 -> package { 'rspec':
   ensure   => 'latest',
   provider => 'gem',
-  install_options   => ['--no-document', '--no-ri']
+  install_options   => ['--no-document']
 }
 -> package { 'sinatra':
   ensure   => 'latest',
   provider => 'gem',
-  install_options   => ['--no-document', '--no-ri']
+  install_options   => ['--no-document']
 }
 -> package { 'unicorn':
   ensure   => 'latest',
   provider => 'gem',
-  install_options   => ['--no-document', '--no-ri']
+  install_options   => ['--no-document']
 }
 -> package { 'bundler':
   ensure   => 'latest',
   provider => 'gem',
-  install_options   => ['--no-document', '--no-ri']
+  install_options   => ['--no-document']
 }
 
