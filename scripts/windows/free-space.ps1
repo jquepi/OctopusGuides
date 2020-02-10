9f3445d18ed9b325ca7b5c2188ff1860c7cc2949
Matthew Casperson <matthewcasperson@gmail.com>
2020-02-10 12:25:13 +1000

    There is no reason to free more space

--- scripts/windows/free-space.ps1
+++ scripts/windows/free-space.ps1
@@ -3,15 +3,16 @@
 # https://gist.github.com/mcasperson/d5abf6990185f0d74bca748f530f43d7 is a break down of the folders and their
 # sizes in a Windows VM
 
-Get-PSDrive C
-
-#Write-Host "Deleting C:\Program Files\boost"
-#rm -Recurse -Force "C:\Program Files\boost"
-Write-Host "Deleting C:\Program Files\unity"
-rm -Recurse -Force "C:\Program Files\unity"
-Write-Host "Deleting c:\Program Files (x86)\Android"
-rm -Recurse -Force "c:\Program Files (x86)\Android"
-Write-Host "Deleting c:\Modules"
-rm -Recurse -Force "c:\Modules"
+# Note that VMs now appear to have plenty of free space, so we keep the free space check only rather
+# then delete files
 
 Get-PSDrive C
+
+#Write-Host "Deleting C:\Program Files\unity"
+#rm -Recurse -Force "C:\Program Files\unity"
+#Write-Host "Deleting c:\Program Files (x86)\Android"
+#rm -Recurse -Force "c:\Program Files (x86)\Android"
+#Write-Host "Deleting c:\Modules"
+#rm -Recurse -Force "c:\Modules"
+
+#Get-PSDrive C
