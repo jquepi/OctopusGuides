0a6409be7f7ccf5b2346e488f78c8145ecb14b09
Matthew Casperson <matthewcasperson@gmail.com>
2020-02-11 11:41:18 +1000

    Installing ruby again as a package

--- puppet/linux/ruby.pp
+++ puppet/linux/ruby.pp
@@ -1,16 +1,23 @@
-exec { 'Install rvm keys':
-  command   => '/usr/bin/gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB',
-  logoutput => true
+package { 'ruby':
+  ensure => 'latest'
 }
--> exec { 'Install rvm':
-  command   => '/usr/bin/curl -sSL https://get.rvm.io | bash -s stable',
-  logoutput => true
+-> package { 'ruby-dev':
+  ensure => 'latest'
 }
--> exec { 'Install ruby':
-  command   => '/usr/local/rvm/bin/rvm install ruby-2.6.3',
-  logoutput => true
-}
--> exec { 'Default ruby':
-  command   => '/usr/local/rvm/bin/rvm alias create default ruby-2.6.3',
-  logoutput => true
-}
+
+# exec { 'Install rvm keys':
+#   command   => '/usr/bin/gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB',
+#   logoutput => true
+# }
+# -> exec { 'Install rvm':
+#   command   => '/usr/bin/curl -sSL https://get.rvm.io | bash -s stable',
+#   logoutput => true
+# }
+# -> exec { 'Install ruby':
+#   command   => '/usr/local/rvm/bin/rvm install ruby-2.6.3',
+#   logoutput => true
+# }
+# -> exec { 'Default ruby':
+#   command   => '/usr/local/rvm/bin/rvm alias create default ruby-2.6.3',
+#   logoutput => true
+# }
