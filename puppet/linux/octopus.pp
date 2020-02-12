e334d55be686891193d8b1562634f8102a760c7c
Matthew Casperson <matthewcasperson@gmail.com>
2020-02-12 15:54:02 +1000

    Prevent images from being removed on restart

--- puppet/linux/octopus.pp
+++ puppet/linux/octopus.pp
@@ -9,18 +9,27 @@ docker_network { 'octopus':
   image_tag => '2017-latest-ubuntu'
 }
 -> docker::run { 'mssql':
-  image => 'mcr.microsoft.com/mssql/server:2017-latest-ubuntu',
-  env   => ['ACCEPT_EULA=Y', 'SA_PASSWORD=Password01!', 'MSSQL_PID=Express'],
-  ports => ['1433:1433'],
-  net   => 'octopus'
+  image                     => 'mcr.microsoft.com/mssql/server:2017-latest-ubuntu',
+  env                       => ['ACCEPT_EULA=Y', 'SA_PASSWORD=Password01!', 'MSSQL_PID=Express'],
+  ports                     => ['1433:1433'],
+  net                       => 'octopus',
+  remove_container_on_start => false,
+  remove_volume_on_start    => false,
+  remove_container_on_stop  => false,
+  remove_volume_on_stop     => false,
 }
 -> docker::image { 'octopusdeploy/linuxoctopus':
   image_tag => '2019.13.4'
 }
 -> docker::run { 'octopusdeploy':
-  image => 'octopusdeploy/linuxoctopus:2019.13.4',
-  env   => ['ADMIN_USERNAME=admin', 'ADMIN_EMAIL=octopusguides@gmail.com', 'ADMIN_PASSWORD=Password01!', 'ACCEPT_EULA=Y'
+  image                     => 'octopusdeploy/linuxoctopus:2019.13.4',
+  env                       => ['ADMIN_USERNAME=admin', 'ADMIN_EMAIL=octopusguides@gmail.com',
+    'ADMIN_PASSWORD=Password01!', 'ACCEPT_EULA=Y'
     , 'CONNSTRING=Server=mssql,1433;Database=Octopus;User Id=SA;Password=Password01!'],
-  ports => ['80:8080', '10943:10943'],
-  net   => 'octopus'
+  ports                     => ['80:8080', '10943:10943'],
+  net                       => 'octopus',
+  remove_container_on_start => false,
+  remove_volume_on_start    => false,
+  remove_container_on_stop  => false,
+  remove_volume_on_stop     => false,
 }
