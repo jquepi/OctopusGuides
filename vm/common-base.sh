c2908c829a55882e4ea7dfbc1a54ebacd4591b47
Matthew Casperson <matthewcasperson@gmail.com>
2020-02-14 08:02:00 +1000

    Added a wait step

--- vm/common-base.sh
+++ vm/common-base.sh
@@ -3,3 +3,6 @@
 # Installs a common base set of applications to allow other images to be quickly spun up
 cd ..
 ./install.sh docker.pp dotnetutilities.pp utilities.pp octopus.pp octopuscli.pp octopusicon.pp
+
+# Wait for Octopus
+./scripts/linux/wait-octopus.sh
