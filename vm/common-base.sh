9f32286c0577e9aef5289c24a7d6c0182ca67ef7
Matthew Casperson <matthewcasperson@gmail.com>
2020-02-14 07:55:45 +1000

    Added a common base VM

---
+++ vm/common-base.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Installs a common base set of applications to allow other images to be quickly spun up
+
+./install.sh docker.pp dotnetutilities.pp utilities.pp octopus.pp octopuscli.pp octopusicon.pp
