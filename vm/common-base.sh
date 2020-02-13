f2c1b18d8b83ada39e41d44d3e20089b9e01e76b
Matthew Casperson <matthewcasperson@gmail.com>
2020-02-14 07:58:57 +1000

    Fixed path

--- vm/common-base.sh
+++ vm/common-base.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # Installs a common base set of applications to allow other images to be quickly spun up
-
+cd ..
 ./install.sh docker.pp dotnetutilities.pp utilities.pp octopus.pp octopuscli.pp octopusicon.pp
