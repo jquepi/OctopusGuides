483cce6cd7e98ad7f1f0a0a4d5b57ee39a7a956a
Matthew Casperson <matthewcasperson@gmail.com>
2020-02-14 10:57:06 +1000

    Added more icons and set the favourites

--- puppet/linux/tomcaticon.pp
+++ puppet/linux/tomcaticon.pp
@@ -1,16 +1,38 @@
-file { '/etc/skel':
-  ensure => 'directory'
-}
--> file { '/etc/skel/Desktop':
-  ensure => 'directory'
-}
--> file { '/etc/skel/Desktop/Tomcat.desktop':
+file { '/usr/share/applications/Tomcat.desktop':
   content => @("EOL"/L)
     [Desktop Entry]
     Version=1.0
     Name=Tomcat
     Comment=Username: tomcat Password: Password01!
     Exec=firefox http://localhost:9091/manager
+    Terminal=false
+    X-MultipleArgs=false
+    Type=Application
+    Icon=firefox
+    Categories=Internet
+    | EOL
+}
+
+file { '/usr/share/applications/Random Quotes Dev.desktop':
+  content => @("EOL"/L)
+    [Desktop Entry]
+    Version=1.0
+    Name=Random Quotes Dev
+    Exec=firefox http://localhost:9091/randomquotes-dev
+    Terminal=false
+    X-MultipleArgs=false
+    Type=Application
+    Icon=firefox
+    Categories=Internet
+    | EOL
+}
+
+file { '/usr/share/applications/Random Quotes Test.desktop':
+  content => @("EOL"/L)
+    [Desktop Entry]
+    Version=1.0
+    Name=Random Quotes Test
+    Exec=firefox http://localhost:9091/randomquotes-test
     Terminal=false
     X-MultipleArgs=false
     Type=Application
