e88989d5d37acfea0f2e1d8627257074b36656d3
Matthew Casperson <matthewcasperson@gmail.com>
2020-02-17 15:21:52 +1000

    Added an apt-get update to the base vm

--- vm/common-base.sh
+++ vm/common-base.sh
@@ -2,7 +2,7 @@
 
 # Installs a common base set of applications to allow other images to be quickly spun up
 cd ..
-./install.sh docker.pp dotnetutilities.pp utilities.pp octopus.pp octopuscli.pp octopusicon.pp desktop.pp
+./install.sh update.pp docker.pp dotnetutilities.pp utilities.pp octopus.pp octopuscli.pp octopusicon.pp desktop.pp
 
 # Wait for Octopus
 ./scripts/linux/wait-octopus.sh
