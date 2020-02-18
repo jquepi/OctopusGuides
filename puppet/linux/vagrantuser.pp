44129064506bd2a81c7da40c4f975b4e4d86f7fa
Matthew Casperson <matthewcasperson@gmail.com>
2020-02-19 09:05:42 +1000

    Generate the flat config file

--- puppet/linux/vagrantuser.pp
+++ puppet/linux/vagrantuser.pp
@@ -8,49 +8,13 @@ User <| title == vagrant |>
 file { '/home/vagrant/.kube':
   ensure => 'directory',
 }
+-> exec { 'Create config file':
+  command   => '/usr/bin/kubectl get config --flatten > /home/vagrant/.kube/config',
+  logoutput => true
+}
 -> file { '/home/vagrant/.kube/config':
-  ensure => present,
-  source => "/root/.kube/config",
-  owner  => 'vagrant',
-  group  => 'vagrant'
-}
--> file_line { 'Update certificate authority':
-  path    => '/home/vagrant/.kube/config',
-  line    => '    certificate-authority: /root/.minikube/ca.crt',
-  match   => '    certificate-authority: /home/vagrant/.minikube/ca.crt',
-  replace => true
-}
--> file_line { 'Update client certificate':
-  path    => '/home/vagrant/.kube/config',
-  line    => '    client-certificate: /root/.minikube/client.crt',
-  match   => '    client-certificate: /home/vagrant/.minikube/ca.crt',
-  replace => true
-}
--> file_line { 'Update client key':
-  path    => '/home/vagrant/.kube/config',
-  line    => '    client-key: /root/.minikube/client.key',
-  match   => '    client-key: /home/vagrant/.minikube/client.key',
-  replace => true
-}
-
-file { '/home/vagrant/.minikube':
-  ensure => 'directory',
-}
--> file { '/home/vagrant/.minikube/client.crt':
-  ensure => present,
-  source => "/root/.minikube/client.crt",
-  owner  => 'vagrant',
-  group  => 'vagrant'
-}
--> file { '/home/vagrant/.minikube/client.key':
-  ensure => present,
-  source => "/root/.minikube/client.key",
-  owner  => 'vagrant',
-  group  => 'vagrant'
-}
--> file { '/home/vagrant/.minikube/ca.crt':
-  ensure => present,
-  source => "/root/.minikube/ca.crt",
-  owner  => 'vagrant',
-  group  => 'vagrant'
+  ensure => 'file',
+  owner => 'vagrant',
+  group => 'vagrant',
+  mode => 0600,
 }
