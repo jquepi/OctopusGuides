c6a953d13c836dbaf2f3eef37afb3e9ceca76010
Matthew Casperson <matthewcasperson@gmail.com>
2020-02-19 10:34:54 +1000

    Changed kube config to use localhost

--- puppet/linux/vagrantuser.pp
+++ puppet/linux/vagrantuser.pp
@@ -12,6 +12,12 @@ file { '/home/vagrant/.kube':
   command   => '/usr/bin/kubectl --kubeconfig /root/.kube/config config view --flatten > /home/vagrant/.kube/config',
   logoutput => true
 }
+-> file_line { 'Change minikube host':
+  path    => '/home/vagrant/.kube/config',
+  line    => '    server: https://localhost:8443',
+  match   => '    server: .*',
+  replace => true,
+}
 -> file { '/home/vagrant/.kube/config':
   ensure => 'file',
   owner  => 'vagrant',
