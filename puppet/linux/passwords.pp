b0e47c03fc111d91064e7aca9ba5390b657c504e
Matthew Casperson <matthewcasperson@gmail.com>
2020-02-19 10:53:45 +1000

    Added some useful commands to get started

--- puppet/linux/passwords.pp
+++ puppet/linux/passwords.pp
@@ -1,9 +1,20 @@
 file { '/usr/share/doc/guidescreds.txt':
   content => @("EOL"/L)
+    Credentials
+    ===========
+
     System                                            vagrant   vagrant
     Octopus         http://localhost                  admin     Password01!
     Jenkins         http://localhost:8080             admin     Password01!
     Tomcat Manager  https://localhost:9091/manager    tomcat    Password01!
+
+    Useful Commands
+    ===============
+
+    Minikube IP:        sudo -H minikube ip                           This IP exposes the Kubernetes deployments (e.g. http://172.29.74.77:30001)
+    List K8S Pods:      kubectl get pods --all-namespaces             This lists all the pods deployed to minikube
+    Show Octopus logs:  docker logs -f octopusdeploy                  Octopus is deployed as a Docker container, and this command will tail the log output
+    Restart Octopus:    sudo systemctl restart docker-octopusdeploy   The Octopus Docker container is managed as a systemd service
     | EOL
 }
 
--- vm/java-jenkins-k8s.sh
+++ vm/java-jenkins-k8s.sh
@@ -19,7 +19,7 @@ export STEP_PAUSE=1000
 
 export BROWSER_TYPE=ChromeHeadlessNoImplicitWaitLambda
 
-export CUCUMBER_TAGS="(not @browserspecific or @firefox) and (not @destinationspecific or @k8s) and (not @repositoryspecific or @localdockerrepo) and (not @applicationspecific or @java) and (not @sourcespecific or @git)"
+export CUCUMBER_TAGS="(not @browserspecific or @chrome) and (not @destinationspecific or @k8s) and (not @repositoryspecific or @localdockerrepo) and (not @applicationspecific or @java) and (not @sourcespecific or @git)"
 export CUCUMBER_PLUGIN=pretty
 
 export GIT_URL=https://github.com/OctopusSamples/RandomQuotes-Java.git
