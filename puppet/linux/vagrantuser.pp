6855c01a30cdca10aab106bd49e7137c43dc3063
Matthew Casperson <matthewcasperson@gmail.com>
2020-02-24 08:39:29 +1000

    Removed minikube config copy

--- puppet/linux/vagrantuser.pp
+++ puppet/linux/vagrantuser.pp
@@ -4,23 +4,3 @@
 }
 
 User <| title == vagrant |>
-
-file { '/home/vagrant/.kube':
-  ensure => 'directory',
-}
--> exec { 'Create config file':
-  command   => '/usr/bin/kubectl --kubeconfig /root/.kube/config config view --flatten > /home/vagrant/.kube/config',
-  logoutput => true
-}
--> file_line { 'Change minikube host':
-  path    => '/home/vagrant/.kube/config',
-  line    => '    server: https://localhost:8443',
-  match   => '    server: .*',
-  replace => true,
-}
--> file { '/home/vagrant/.kube/config':
-  ensure => 'file',
-  owner  => 'vagrant',
-  group  => 'vagrant',
-  mode   => '0600',
-}
