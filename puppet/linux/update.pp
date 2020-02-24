bea7156b4a9ffcf8bb7fe131771c29d5982985c8
Matthew Casperson <matthewcasperson@gmail.com>
2020-02-25 08:26:59 +1000

    Updated kernel as part of vagrant build

--- puppet/linux/update.pp
+++ puppet/linux/update.pp
@@ -1,4 +1,4 @@
 exec { "apt-update":
-  command => "/usr/bin/apt-get update; /usr/bin/apt-get upgrade -y",
+  command => "/usr/bin/apt-get update; /usr/bin/apt-get upgrade -y; /usr/bin/apt-get dist-upgrade -y",
   timeout => 3600
 }
