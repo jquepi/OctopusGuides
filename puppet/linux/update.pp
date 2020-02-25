38168828d96c3910cdf53d53ad0ab96000ee981b
Matthew Casperson <matthewcasperson@gmail.com>
2020-02-26 08:45:20 +1000

    Install virtualbox x11 regardless

--- puppet/linux/update.pp
+++ puppet/linux/update.pp
@@ -2,6 +2,9 @@ exec { "add dkms":
   command => "/usr/bin/apt-get update; /usr/bin/apt-get install build-essential dkms linux-headers-$(uname -r) -y",
   timeout => 3600
 }
+-> exec { "add dkms":
+  command => "apt-get install -y -o Dpkg::Options::='--force-confnew' virtualbox-guest-x11",
+}
 -> exec { "apt-update":
   command => "/usr/bin/apt-get update; /usr/bin/apt-get upgrade -y; /usr/bin/apt-get dist-upgrade -y",
   timeout => 3600
