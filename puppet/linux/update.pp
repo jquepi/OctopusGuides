581535c83c4f814a4b1897c4521aa1cf3798f70f
Matthew Casperson <matthewcasperson@gmail.com>
2020-02-25 20:02:34 +1000

    Install dkms before a kernel update

--- puppet/linux/update.pp
+++ puppet/linux/update.pp
@@ -1,4 +1,8 @@
-exec { "apt-update":
+exec { "add dkms":
+  command => "/usr/bin/apt-get update; /usr/bin/apt-get install build-essential dkms linux-headers-$(uname -r) -y",
+  timeout => 3600
+}
+-> exec { "apt-update":
   command => "/usr/bin/apt-get update; /usr/bin/apt-get upgrade -y; /usr/bin/apt-get dist-upgrade -y",
   timeout => 3600
 }
