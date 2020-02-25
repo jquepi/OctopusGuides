6f43d195edfa90ee2eec1f53ba5d8ccd4cda0ca1
Matthew Casperson <matthewcasperson@gmail.com>
2020-02-26 08:45:35 +1000

    Install virtualbox x11 regardless

--- puppet/linux/update.pp
+++ puppet/linux/update.pp
@@ -2,7 +2,7 @@ exec { "add dkms":
   command => "/usr/bin/apt-get update; /usr/bin/apt-get install build-essential dkms linux-headers-$(uname -r) -y",
   timeout => 3600
 }
--> exec { "add dkms":
+-> exec { "add virtualbox x11 driver":
   command => "apt-get install -y -o Dpkg::Options::='--force-confnew' virtualbox-guest-x11",
 }
 -> exec { "apt-update":
