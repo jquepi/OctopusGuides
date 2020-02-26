b3a4a3da6412aba69c041fc7a90e645be37e5d79
Matthew Casperson <matthewcasperson@gmail.com>
2020-02-26 15:57:03 +1000

    Removed an unnecessary timeout

--- puppet/linux/update.pp
+++ puppet/linux/update.pp
@@ -4,7 +4,6 @@ exec { "add dkms":
 }
 -> exec { "add virtualbox x11 driver":
   command => "/usr/bin/apt-get install -y -o Dpkg::Options::='--force-confnew' virtualbox-guest-x11",
-  timeout => 3600
 }
 -> exec { "apt-update":
   command => "/usr/bin/apt-get update; /usr/bin/apt-get upgrade -y; /usr/bin/apt-get dist-upgrade -y",
