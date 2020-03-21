f5a9dc6435a72b62ab8da665cbb23255683a4270
Matthew Casperson <matthewcasperson@gmail.com>
2020-03-22 09:54:45 +1000

    used new az

--- create-azure-web-app.ps1
+++ create-azure-web-app.ps1
@@ -3,15 +3,15 @@ param(
 )
 
 # Print the version info
-az --version
+& "C:\Program Files (x86)\Microsoft SDKs\Azure\CLI2\wbin\az.cmd" --version
 
-$exists = & az group exists --name $name
+$exists = & "C:\Program Files (x86)\Microsoft SDKs\Azure\CLI2\wbin\az.cmd" group exists --name $name
 if ($exists -eq "true") {
     Write-Host "Resource group $name already exists. Deleting."
-    az group delete --name $name --yes
+    & "C:\Program Files (x86)\Microsoft SDKs\Azure\CLI2\wbin\az.cmd" group delete --name $name --yes
 }
 
-az group create --location australiaeast --name $name --tags Lifetime=Permanent OwnerContact=@matthew.casperson
-az appservice plan create --name $name --resource-group $name --number-of-workers 1 --sku B1
-az webapp create -g $name -p $name -n $name --% --runtime "aspnet|V4.7"
-az webapp log config -g $name -n $name --application-logging true --detailed-error-messages true --web-server-logging filesystem
+& "C:\Program Files (x86)\Microsoft SDKs\Azure\CLI2\wbin\az.cmd" group create --location australiaeast --name $name --tags Lifetime=Permanent OwnerContact=@matthew.casperson
+& "C:\Program Files (x86)\Microsoft SDKs\Azure\CLI2\wbin\az.cmd" appservice plan create --name $name --resource-group $name --number-of-workers 1 --sku B1
+& "C:\Program Files (x86)\Microsoft SDKs\Azure\CLI2\wbin\az.cmd" webapp create -g $name -p $name -n $name --% --runtime "aspnet|V4.7"
+& "C:\Program Files (x86)\Microsoft SDKs\Azure\CLI2\wbin\az.cmd" webapp log config -g $name -n $name --application-logging true --detailed-error-messages true --web-server-logging filesystem
