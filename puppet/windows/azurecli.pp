e630dfbadf14f4b72baf3e79e4052818a1662d08
Matthew Casperson <matthewcasperson@gmail.com>
2020-03-22 10:38:15 +1000

    Installed latest az

--- puppet/windows/azurecli.pp
+++ puppet/windows/azurecli.pp
@@ -3,6 +3,6 @@ include chocolatey
 # The GitHub Action VMs come with the azure CLI, but it is an old version (as of March 2020) that
 # fails to deploy web apps. So we manually update
 package { 'azure-cli':
-  ensure   => installed,
+  ensure   => 'latests',
   provider => chocolatey,
 }
