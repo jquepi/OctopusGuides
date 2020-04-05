421319b74c29f9975e8192ef981bf64355be5d94
Matthew Casperson <matthewcasperson@gmail.com>
2020-04-06 06:24:36 +1000

    Removed trigger

--- puppet/linux/bamboo.pp
+++ puppet/linux/bamboo.pp
@@ -5,6 +5,8 @@ archive { '/opt/bamboo.jar':
   allow_insecure => true
 }
 
+# Using the repo dorectly started to result in Unknown date format Bad header data:
+# https://community.developer.atlassian.com/t/the-repository-https-packages-atlassian-com-atlassian-sdk-deb-stable-release-is-not-signed/36901/11
 # apt::key { 'atlassian-repository':
 #   id     => 'D648E5571A519459EC58F888B56C9565957495B9',
 #   source => 'https://packages.atlassian.com/api/gpg/key/public',
@@ -25,6 +27,8 @@ archive { '/opt/bamboo.jar':
 # -> package { 'atlassian-plugin-sdk':
 #   ensure => installed,
 # }
+
+# Download the deb file manually
 archive { '/opt/atlassian-plugin-sdk_8.0.16_all.deb':
   ensure         => present,
   extract        => false,
