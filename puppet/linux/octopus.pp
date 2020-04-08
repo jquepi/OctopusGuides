176546839878b1ac76b4801d5a9c666518f328ac
Matthew Casperson <matthewcasperson@gmail.com>
2020-04-08 10:15:46 +1000

    Moved autoheal to the last step

--- puppet/linux/octopus.pp
+++ puppet/linux/octopus.pp
@@ -64,23 +64,3 @@ docker::image { 'octopusdeploy/octopusdeploy':
   match   => 'Restart=on-failure',
   replace => true,
 }
-
-# Watch for unhealthy Octopus container and restart automatically
-docker::image { 'willfarrell/autoheal':
-  image_tag => 'latest'
-}
--> docker::run { 'autoheal':
-  image            => 'willfarrell/autoheal:latest',
-  extra_parameters => [ '--restart=always' ],
-  volumes          => ['/var/run/docker.sock:/var/run/docker.sock'],
-}
--> exec { 'enable service autoheal':
-  command   => '/bin/systemctl enable docker-autoheal',
-  logoutput => true
-}
--> file_line { 'Autoheal always restart':
-  path    => '/etc/systemd/system/docker-autoheal.service',
-  line    => 'Restart=always',
-  match   => 'Restart=on-failure',
-  replace => true,
-}
