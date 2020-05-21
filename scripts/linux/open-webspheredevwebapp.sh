8f029fd07102b0989f1dcd28b7aff356f406a01d
Matthew Casperson <matthewcasperson@gmail.com>
2020-05-22 07:33:59 +1000

    Working on websphere guide

---
+++ scripts/linux/open-webspheredevwebapp.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+/usr/lib/jvm/java-13-openjdk-amd64/bin/java \
+  "--enable-preview" \
+  "-Xmx2g" \
+  "-Dwebdriver.gecko.driver=/usr/bin/geckodriver" \
+  "-DstepPause=$STEP_PAUSE" \
+  "-DCucumberAlias-GuideSpecificScreenshotDir=$SCREENSHOT_PATH" \
+  "-DCucumberAlias-ExternalMediaPath=$MEDIA_PATH" \
+  "-DmoveCursorToElement=$MOVE_TO_MOUSE_CURSOR" \
+  "-DmouseMoveVerticalOffset=$MOUSE_VERTICAL_OFFSET" \
+  "-DdisableHighlights=$DISABLE_HIGHLIGHTS" \
+  "-DdisableScreenshots=$DISABLE_SCREENSHOTS" \
+  "-DdisableVideoRecording=$DISABLE_VIDEO" \
+  "-DslackHookUrl=$SLACK_HOOK_URL" \
+  "-DscreenshotS3Enabled=$STHREE_SCREENSHOT_ENABLED" \
+  "-DscreenshotS3Bucket=$SCREENSHOT_BUCKET" \
+  "-DstepHandlerMessage=$GITHUB_SHA" \
+  "-DslackStepHandlerEnabled=$SLACK_STEP_HANDLER" \
+  "-DslackStepHandlerErrorOnly=$SLACK_ON_ERROR_ONLY" \
+  "-DCucumberAlias-ExternalUrl=http://localhost:9080/randomquotes-dev/index.html" \
+  "-DCucumberAlias-Screenshot=tomcat-random-quotes-dev-app.png" \
+  "-DCucumberAlias-ExternalBrowserType=$BROWSER_TYPE" \
+  -jar /opt/webdrivertraining.1.0-SNAPSHOT.jar \
+  --plugin "$CUCUMBER_PLUGIN" \
+  --tags "$CUCUMBER_TAGS" \
+  features/octopus/open-randomquotes-generic.feature
