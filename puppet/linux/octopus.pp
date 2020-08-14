83ee009ea6ec0f86c2e722cfbe659a05f47fc116
Matthew Casperson <matthewcasperson@gmail.com>
2020-08-14 00:18:38 +0000

    Disabled DIND

--- puppet/linux/octopus.pp
+++ puppet/linux/octopus.pp
@@ -40,7 +40,7 @@ docker::image { 'octopusdeploy/octopusdeploy':
   env                       => ['ADMIN_USERNAME=admin', 'ADMIN_EMAIL=octopusguides@gmail.com',
     'ADMIN_PASSWORD=Password01!', 'ACCEPT_EULA=Y'
     , 'DB_CONNECTION_STRING=Server=mssql,1433;Database=Octopus;User Id=SA;Password=Password01!;ConnectRetryCount=6',
-    'MASTER_KEY=6EdU6IWsCtMEwk0kPKflQQ=='],
+    'MASTER_KEY=6EdU6IWsCtMEwk0kPKflQQ==', 'DISABLE_DIND=Y'],
   ports                     => ['80:8080', '10943:10943'],
   net                       => 'octopus',
   extra_parameters          => [ '--restart=always' ],
