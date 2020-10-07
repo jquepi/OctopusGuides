c5cdea4a3678a2689344ab585b8794c6ea8dec47
Matthew Casperson <matthewcasperson@gmail.com>
2020-10-08 08:27:25 +1000

    Updated repo

--- puppet/linux/dockerinstall.pp
+++ puppet/linux/dockerinstall.pp
@@ -7,7 +7,7 @@ apt::key { 'docker-repository':
   comment  => 'This is the docker repository',
   location => 'https://download.docker.com/linux/ubuntu',
   release  => '',
-  repos    => 'bionic stable',
+  repos    => 'focal stable',
   key      => {
     'id' => '9DC858229FC7DD38854AE2D88D81803C0EBFCD88',
   },
