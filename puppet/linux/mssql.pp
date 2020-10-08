a5dc346c0d00b0e9300c00f0c672ce61dc2c03d2
Matthew Casperson <matthewcasperson@gmail.com>
2020-10-08 17:49:40 +1000

    Increased the initial docker container boot time

--- puppet/linux/mssql.pp
+++ puppet/linux/mssql.pp
@@ -30,3 +30,13 @@ docker_network { 'octopus':
   line                  => 'Restart=always',
   match                 => 'Restart=on-failure',
 }
+-> exec { 'enable service docker-mssql':
+  command   => '/bin/systemctl enable docker-mssql',
+  logoutput => true
+}
+
+# For some reason the initial boot of the mssql container fails in hyperv. The logs at https://gist.github.com/mcasperson/d642c27797bdd5cb478fc01394d89230
+# are an example of this. The workaround is to stop the service, kill the container, and restart the service. Restarting the
+# container leaves it in a corrupted state, and and subsequent restarts fail. The solution for now is to kill the docker-mssql service,
+# stop and remove the mssql container, and restart the docker-mssql service. This second restart works fine, and the
+# docker-octopusdeploy service can be restarted successfully.
