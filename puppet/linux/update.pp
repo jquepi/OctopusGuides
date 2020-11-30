42c6e793196b02338ccbe4ce8c68a24a1a5c9e98
Matthew Casperson <matthewcasperson@gmail.com>
2020-11-30 16:10:59 +1000

    Fixed package upgrade comands

--- puppet/linux/update.pp
+++ puppet/linux/update.pp
@@ -5,8 +5,9 @@ exec { "add dkms":
 -> exec { "add virtualbox x11 driver":
   command => "/usr/bin/apt-get install -y -o Dpkg::Options::='--force-confnew' virtualbox-guest-x11",
 }
+# -o Dpkg::Options::='--force-confnew' is used to retain the changes to the resolved.conf file
 -> exec { "apt-update":
-  command => "/usr/bin/apt-get update; /usr/bin/apt-get upgrade -y; /usr/bin/apt-get dist-upgrade -y",
+  command => "/usr/bin/apt-get update; /usr/bin/apt-get upgrade -y; /usr/bin/apt-get dist-upgrade -y -o Dpkg::Options::='--force-confold'",
   timeout => 3600
 }
 -> exec { "Disable the existing virtualbox service":
