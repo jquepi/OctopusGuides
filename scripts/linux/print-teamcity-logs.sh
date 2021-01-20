afe78b34f565b1d07b675c517502829b1e4fcb4a
Matthew Casperson <matthewcasperson@gmail.com>
2021-01-20 11:26:02 +1000

    Fixed log printing

--- scripts/linux/print-teamcity-logs.sh
+++ scripts/linux/print-teamcity-logs.sh
@@ -1,2 +1,3 @@
-cat /opt/TeamCity/logs/teamcity-agent.log
-cat /opt/TeamCity/logs/teamcity-vcs.log
+sudo ls /opt/TeamCity/logs/
+sudo cat /opt/TeamCity/logs/teamcity-agent.log
+sudo cat /opt/TeamCity/logs/teamcity-vcs.log
