a0841c7e62551353ae28358ca1c2486af48567c6
Matthew Casperson <matthewcasperson@gmail.com>
2021-02-28 13:19:37 +1000

    try lower case "v"

--- create-azure-web-app.ps1
+++ create-azure-web-app.ps1
@@ -14,7 +14,7 @@ if ($exists -eq "true") {
 
 & "az" group create --location australiaeast --name $name --tags Lifetime=Permanent OwnerContact=@matthew.casperson
 & "az" appservice plan create --name $name --resource-group $name --number-of-workers 1 --sku B1
-& "az" webapp create -g $name -p $name -n $name --% --runtime "aspnet|V4.7"
+& "az" webapp create -g $name -p $name -n $name --% --runtime "aspnet|v4.7"
 & "az" webapp log config -g $name -n $name --detailed-error-messages true --web-server-logging filesystem
 
 if ($LASTEXITCODE -ne 0) { throw "Exit code is $LASTEXITCODE" }
