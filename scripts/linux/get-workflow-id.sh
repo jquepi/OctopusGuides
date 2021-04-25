f7db9df59b590175c298047ded64c433df666be5
Matthew Casperson <matthewcasperson@gmail.com>
2021-04-25 13:39:43 +1000

    Moved workflow ID lookup to script

--- scripts/linux/get-workflow-id.sh
+++ scripts/linux/get-workflow-id.sh
@@ -2,7 +2,7 @@
 
 for (( c=1; c<=10; c++ ))
 do
-  WORKFLOW_ID=$(curl -s "https://api.github.com/repos/OctopusDeploy/OctopusGuides/actions/workflows?per_page=100&page=$c" | jq -c '.workflows[] | select(.name == "$GITHUB_WORKFLOW") | .id')
+  WORKFLOW_ID=$(curl -s "https://api.github.com/repos/OctopusDeploy/OctopusGuides/actions/workflows?per_page=100&page=$c" | jq -c ".workflows[] | select(.name == \"$GITHUB_WORKFLOW\") | .id")
   if [[ -n $WORKFLOW_ID ]]; then
     # There are cases were the workflow name has been registered more than once
     # by mistake. Assume the first one is correct.
