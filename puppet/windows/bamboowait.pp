76cb3b5314022661cc06c803e38bb994050eb1cf
Matthew Casperson <matthewcasperson@gmail.com>
2021-09-09 09:08:51 +1000

    Fixed up a wait time for bamboo

--- puppet/windows/bamboowait.pp
+++ puppet/windows/bamboowait.pp
@@ -1,5 +1,5 @@
 exec { 'Wait For Bamboo':
-  command   => 'for ($x = 0; $x -lt 15; ++$x) {if ((Test-NetConnection -ComputerName localhost -Port 6990).TcpTestSucceeded) {exit 0;}; Write-Host "Sleeping...";} exit 1;',
+  command   => 'for ($x = 0; $x -lt 10; ++$x) {if ((Test-NetConnection -ComputerName localhost -Port 6990).TcpTestSucceeded) {exit 0;}; Write-Host "Sleeping..."; Sleep 60} exit 1;',
   provider  => powershell,
   logoutput => true,
   timeout   => 600,
