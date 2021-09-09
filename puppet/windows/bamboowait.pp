80b69d41ff87c275aea4c9d633e1fc2d31aa4a26
Matthew Casperson <matthewcasperson@gmail.com>
2021-09-10 07:39:35 +1000

    Captured the bamboo logs

--- puppet/windows/bamboowait.pp
+++ puppet/windows/bamboowait.pp
@@ -1,4 +1,8 @@
-exec { 'Wait For Bamboo':
+exec { 'Get logs':
+  command   => 'cat amps-standalone-bamboo-LATEST\target\bamboo-LATEST.log',
+  logoutput => true
+}
+-> exec { 'Wait For Bamboo':
   command   => 'for ($x = 0; $x -lt 10; ++$x) {if ((Test-NetConnection -ComputerName localhost -Port 6990).TcpTestSucceeded) {exit 0;}; Write-Host "Sleeping..."; Sleep 60} exit 1;',
   provider  => powershell,
   logoutput => true,
