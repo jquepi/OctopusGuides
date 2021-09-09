04c0904a9518321f58104f6e5e4dd28bf375b20e
Matthew Casperson <matthewcasperson@gmail.com>
2021-09-10 07:07:20 +1000

    Using older version of java

--- puppet/windows/bamboo.pp
+++ puppet/windows/bamboo.pp
@@ -14,6 +14,14 @@ file { 'C:/atlas':
   creates      => 'C:/atlas/installtype.txt',
   cleanup      => true,
 }
+-> archive { 'C:/tools/zulu8.46.0.19-ca-jdk8.0.252-win_x64.zip':
+  ensure       => present,
+  extract      => true,
+  extract_path => 'C:/',
+  source       => 'https://octopus-guides.s3.amazonaws.com/java/zulu8.46.0.19-ca-jdk8.0.252-win_x64.zip',
+  creates      => 'C:/zulu8.46.0.19-ca-jdk8.0.252-win_x64/release',
+  cleanup      => true,
+}
 -> file { 'C:/start_bamboo.ps1':
   ensure  => 'file',
   owner   => 'Administrators',
@@ -22,9 +30,14 @@ file { 'C:/atlas':
   content => @(EOT)
     ls c:\atlas
 
-    # Bamboo only supports java 8
-    $path = Get-ChildItem "C:\Program Files\Java\" | ?{$_.Name -like "jdk1.8.0_*"} | Sort -Descending | Select -First 1 | Select -ExpandProperty FullName
-    $env:JAVA_HOME=$path
+    # Bamboo only supports java 8, and only versions lower than 255
+    # Caused by: java.lang.IllegalArgumentException: Invalid version number: Version number may be negative or greater than 255
+    #    at com.ibm.icu.util.VersionInfo.getInstance(VersionInfo.java:191)
+    # See https://stackoverflow.com/questions/64040255/invalid-version-number-version-number-may-be-negative-or-greater-than-255
+
+    #$path = Get-ChildItem "C:\Program Files\Java\" | ?{$_.Name -like "jdk1.8.0_*"} | Sort -Descending | Select -First 1 | Select -ExpandProperty FullName
+
+    $env:JAVA_HOME="C:/zulu8.46.0.19-ca-jdk8.0.252-win_x64"
     # Launch Bamboo in a background process
     start "c:\atlas\bin\atlas-run-standalone.bat" -ArgumentList @("--product", "bamboo") -PassThru
     New-Item -ItemType file c:\BambooStarted.txt
