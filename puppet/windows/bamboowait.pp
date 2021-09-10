aaf41c26fedc0d91e84b56abd149ebfe94d7e64d
Matthew Casperson <matthewcasperson@gmail.com>
2021-09-10 13:01:23 +1000

    Skip the test for now

--- puppet/windows/bamboowait.pp
+++ puppet/windows/bamboowait.pp
@@ -18,9 +18,9 @@ exec { 'Get files':
   provider  => powershell,
   logoutput => true
 }
--> exec { 'Wait For Bamboo':
-  command   => 'for ($x = 0; $x -lt 10; ++$x) {if ((Test-NetConnection -ComputerName localhost -Port 6990).TcpTestSucceeded) {exit 0;}; Write-Host "Sleeping..."; Sleep 60} exit 1;',
-  provider  => powershell,
-  logoutput => true,
-  timeout   => 600,
-}
+#-> exec { 'Wait For Bamboo':
+#  command   => 'for ($x = 0; $x -lt 10; ++$x) {if ((Test-NetConnection -ComputerName localhost -Port 6990).TcpTestSucceeded) {exit 0;}; Write-Host "Sleeping..."; Sleep 60} exit 1;',
+#  provider  => powershell,
+#  logoutput => true,
+#  timeout   => 600,
+#}
