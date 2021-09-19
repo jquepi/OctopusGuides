2a326017e383a55081bfdd458a35f8a20b76a1cc
Matthew Casperson <matthewcasperson@gmail.com>
2021-09-20 08:08:34 +1000

    Manually install jenkins

--- puppet/windows/jenkinsinstall.pp
+++ puppet/windows/jenkinsinstall.pp
@@ -1,5 +1,26 @@
 # Install this early, as it takes a few minutes to initilaize
-package { 'jenkins':
-  ensure   => installed,
-  provider => chocolatey
+# Jenkins in chocolaty hasn't been updated in quite a while, so manually install
+
+download_file {'jenkins.msi':
+  destination_directory => 'C:/tools',
+  url => 'https://octopus-guides.s3.amazonaws.com/jenkins/jenkins.msi'
+}
+-> file { 'C:/start_jenkins.ps1':
+  ensure  => 'file',
+  owner   => 'Administrators',
+  group   => 'Administrators',
+  mode    => '0644',
+  content => @(EOT)
+    start-process msiexec -Wait -ArgumentList @("/qn", "/norestart", "/l*v", "c:\jenkinsinstall.log", "/i", "c:\tools\jenkins.msi")
+    Get-Content c:\jenkinsinstall.log
+    net start jenkins
+    New-Item -ItemType file c:\JenkinsStarted.txt
+    exit 0
+    | EOT
+}
+-> exec { 'Start Jenkins':
+  command   => '& C:/start_jenkins.ps1',
+  creates   => 'C:/JenkinsStarted.txt',
+  provider  => powershell,
+  logoutput => true
 }
