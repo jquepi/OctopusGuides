4818ec996344356a5c051b063d55909b58230580
Matthew Casperson <matthewcasperson@gmail.com>
2021-11-05 11:31:42 +1000

    Disabling azure cli install

--- puppet/windows/azurecli.pp
+++ puppet/windows/azurecli.pp
@@ -2,7 +2,7 @@ include chocolatey
 
 # The GitHub Action VMs come with the azure CLI, but it is an old version (as of March 2020) that
 # fails to deploy web apps. So we manually update. Note 2.30.0 had issues installing.
-//package { 'azure-cli':
-//  ensure   => '2.29.2',
-//  provider => chocolatey,
-//}
+#package { 'azure-cli':
+#  ensure   => '2.29.2',
+#  provider => chocolatey,
+#}
