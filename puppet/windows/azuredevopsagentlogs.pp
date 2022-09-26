5e243cb5231a46ba3c3517049aff3fe35cd9f7d6
Matthew Casperson <matthewcasperson@gmail.com>
2022-09-26 10:22:30 +1000

    remove the creates statement so the logs are actually run

--- puppet/windows/azuredevopsagentlogs.pp
+++ puppet/windows/azuredevopsagentlogs.pp
@@ -1,6 +1,5 @@
 exec { 'Get agent logs':
   command   => 'Get-ChildItem "C:\tools\vsts-agent\_diag" -Filter *.log | % {Get-Content $_.FullName}',
-  creates   => 'c:/AzureAgentStarted.txt',
   provider  => powershell,
   logoutput => true
 }
