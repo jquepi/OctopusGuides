ca5276c5fc20bd2aa3f9c2350e832b7dcccea530
Matthew Casperson <matthewcasperson@gmail.com>
2022-09-30 13:35:24 +1000

    Dump more debug info

--- puppet/windows/azuredevopsagentlogs.pp
+++ puppet/windows/azuredevopsagentlogs.pp
@@ -1,5 +1,16 @@
-exec { 'Get agent logs':
-  command   => 'Get-ChildItem "C:\tools\vsts-agent\_diag" -Filter *.log | % {Get-Content $_.FullName}',
+file { 'C:/azure_devops_logs.ps1':
+  ensure  => 'file',
+  owner   => 'Administrators',
+  group   => 'Administrators',
+  mode    => '0644',
+  content => @(EOT)
+    ls C:\tools\vsts-agent
+    ls C:\tools\vsts-agent\_diag
+    Get-ChildItem "C:\tools\vsts-agent\_diag" -Filter *.log | % {Get-Content $_.FullName}
+    | EOT
+}
+-> exec { 'Populate TFVC repo':
+  command   => '& C:/azure_devops_logs.ps1',
   provider  => powershell,
   logoutput => true
 }
