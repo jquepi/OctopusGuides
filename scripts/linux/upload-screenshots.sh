287e710d3c7ef423c64c1d5a1d08b32abfaaaaa1
Matthew Casperson <matthewcasperson@gmail.com>
2022-10-26 14:21:14 +1000

    Fixed file copy

--- scripts/linux/upload-screenshots.sh
+++ scripts/linux/upload-screenshots.sh
@@ -5,7 +5,7 @@ find $MEDIA_PATH -name '*.png' -type f -exec bash -c 'cwebp -q 80 "$0" -o "${0%.
 
 # Copy all images to a new dir called resized
 mkdir $MEDIA_PATH/resized
-cp -R $MEDIA_PATH $MEDIA_PATH/resized
+rsync -a $MEDIA_PATH/ $MEDIA_PATH/resized/
 
 # Resize all the image in the new dir with a width of 800 pixels
 find $MEDIA_PATH/resized -name '*.*' -type f -exec bash -c 'mogrify -resize 800x "$0"' {} \;
